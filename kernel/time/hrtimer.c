9376e9a05098ef4c89131e3029598265409321b6
Anna-Maria Gleixner <anna-maria@linutronix.de>
2017-12-20 17:13:08 +0100

    hrtimer: Unify handling of hrtimer remove
    When the first hrtimer on the current CPU is removed,
    hrtimer_force_reprogram() is invoked but only when
    CONFIG_HIGH_RES_TIMERS=y and hrtimer_cpu_base.hres_active is set.
    
    hrtimer_force_reprogram() updates hrtimer_cpu_base.expires_next and
    reprograms the clock event device. When CONFIG_HIGH_RES_TIMERS=y and
    hrtimer_cpu_base.hres_active is set, a pointless hrtimer interrupt can be
    prevented.
    
    hrtimer_check_target() makes the 'can remote enqueue' decision. As soon as
    hrtimer_check_target() is unconditionally available and
    hrtimer_cpu_base.expires_next is updated by hrtimer_reprogram(),
    hrtimer_force_reprogram() needs to be available unconditionally as well to
    prevent the following scenario with CONFIG_HIGH_RES_TIMERS=n:
    
    - the first hrtimer on this CPU is removed and hrtimer_force_reprogram() is
      not executed
    
    - CPU goes idle (next timer is calculated and hrtimers are taken into
      account)
    
    - a hrtimer is enqueued remote on the idle CPU: hrtimer_check_target()
      compares expiry value and hrtimer_cpu_base.expires_next. The expiry value
      is after expires_next, so the hrtimer is enqueued. This timer will fire
      late, if it expires before the effective first hrtimer on this CPU and
      the comparison was with an outdated expires_next value.
    
    To prevent this scenario, make hrtimer_force_reprogram() unconditional
    except the effective reprogramming part, which gets eliminated by the
    compiler in the CONFIG_HIGH_RES_TIMERS=n case.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -546,9 +546,6 @@ hrtimer_force_reprogram(struct hrtimer_cpu_base *cpu_base, int skip_equal)
 {
 	ktime_t expires_next;
 
-	if (!__hrtimer_hres_active(cpu_base))
-		return;
-
 	expires_next = __hrtimer_get_next_event(cpu_base, NULL);
 
 	if (skip_equal && expires_next == cpu_base->expires_next)
@@ -557,6 +554,9 @@ hrtimer_force_reprogram(struct hrtimer_cpu_base *cpu_base, int skip_equal)
 	cpu_base->expires_next = expires_next;
 
 	/*
+	 * If hres is not active, hardware does not have to be
+	 * reprogrammed yet.
+	 *
 	 * If a hang was detected in the last timer interrupt then we
 	 * leave the hang delay active in the hardware. We want the
 	 * system to make progress. That also prevents the following
@@ -570,7 +570,7 @@ hrtimer_force_reprogram(struct hrtimer_cpu_base *cpu_base, int skip_equal)
 	 * set. So we'd effectivly block all timers until the T2 event
 	 * fires.
 	 */
-	if (cpu_base->hang_detected)
+	if (!__hrtimer_hres_active(cpu_base) || cpu_base->hang_detected)
 		return;
 
 	tick_program_event(cpu_base->expires_next, 1);
@@ -863,14 +863,21 @@ static void __remove_hrtimer(struct hrtimer *timer,
 			     u8 newstate, int reprogram)
 {
 	struct hrtimer_cpu_base *cpu_base = base->cpu_base;
+	u8 state = timer->state;
 
-	if (!(timer->state & HRTIMER_STATE_ENQUEUED))
-		goto out;
+	/*
+	 * Pairs with the lockless read in hrtimer_is_queued()
+	 *
+	 * We need to preserve PINNED state here, otherwise we may end up
+	 * migrating pinned hrtimers as well.
+	 */
+	WRITE_ONCE(timer->state, newstate | (state & HRTIMER_STATE_PINNED));
+	if (!(state & HRTIMER_STATE_ENQUEUED))
+		return;
 
 	if (!timerqueue_del(&base->active, &timer->node))
 		cpu_base->active_bases &= ~(1 << base->index);
 
-#ifdef CONFIG_HIGH_RES_TIMERS
 	/*
 	 * Note: If reprogram is false we do not update
 	 * cpu_base->next_timer. This happens when we remove the first
@@ -881,16 +888,6 @@ static void __remove_hrtimer(struct hrtimer *timer,
 	 */
 	if (reprogram && timer == cpu_base->next_timer)
 		hrtimer_force_reprogram(cpu_base, 1);
-#endif
-
-out:
-	/*
-	* We need to preserve PINNED state here, otherwise we may end up
-	* migrating pinned hrtimers as well.
-	*/
-	/* Pairs with the lockless read in hrtimer_is_queued() */
-	WRITE_ONCE(timer->state,
-		   newstate | (timer->state & HRTIMER_STATE_PINNED));
 }
 
 /*
@@ -1575,9 +1572,9 @@ long hrtimer_nanosleep(const struct timespec64 *rqtp,
 	}
 
 	restart = &current->restart_block;
+	restart->fn = hrtimer_nanosleep_restart;
 	restart->nanosleep.clockid = t.timer.base->clockid;
 	restart->nanosleep.expires = hrtimer_get_expires_tv64(&t.timer);
-	set_restart_fn(restart, hrtimer_nanosleep_restart);
 out:
 	destroy_hrtimer_on_stack(&t.timer);
 	return ret;
