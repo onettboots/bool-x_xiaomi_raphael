a8dafa379acc680650a8e00e0032be4984587d65
Matthew Wilcox <mawilcox@microsoft.com>
2018-01-31 16:18:58 -0800

    UPSTREAM: mm: introduce _slub_counter_t
    Instead of putting the ifdef in the middle of the definition of struct
    page, pull it forward to the rest of the ifdeffery around the SLUB
    cmpxchg_double optimisation.
    
    Link: http://lkml.kernel.org/r/20171220155552.15884-6-willy@infradead.org
    Change-Id: I42672d62aebb00a0295231e262942d44174b9ed5
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -72,18 +72,7 @@ struct page {
 	};
 
 	union {
-#if defined(CONFIG_HAVE_CMPXCHG_DOUBLE) && \
-	defined(CONFIG_HAVE_ALIGNED_STRUCT_PAGE)
-		/* Used for cmpxchg_double in slub */
-		unsigned long counters;
-#else
-		/*
-		 * Keep _refcount separate from slub cmpxchg_double data.
-		 * As the rest of the double word is protected by slab_lock
-		 * but _refcount is not.
-		 */
-		unsigned counters;
-#endif
+		_slub_counter_t counters;
 		unsigned int active;		/* SLAB */
 		struct {			/* SLUB */
 			unsigned inuse:16;
