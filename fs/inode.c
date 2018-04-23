f966a56af7a6ebd54e0485fb7760510a973eaf54
Deepa Dinamani <deepa.kernel@gmail.com>
2018-04-22 20:18:46 -0700

    BACKPORT: fs: add timespec64_truncate()
    As vfs moves to using struct timespec64 to represent times,
    update the argument to timespec_truncate() to use
    struct timespec64. Also change the name of the function.
    The rest of the implementation logic is the same.
    
    Move this to fs/inode.c instead of kernel/time/time.c as all the
    users of this api are filesystems.
    
    Change-Id: I19b58d7d1d5ec263b04237a33b8f981e7d407171
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Cc: <viro@zeniv.linux.org.uk>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/inode.c
+++ fs/inode.c
@@ -2173,6 +2173,29 @@ EXPORT_SYMBOL(inode_nohighmem);
  * Truncate a timespec64 to a granularity. Always rounds down. gran must
  * not be 0 nor greater than a second (NSEC_PER_SEC, or 10^9 ns).
  */
+struct timespec64 timespec64_trunc(struct timespec64 t, unsigned gran)
+{
+	/* Avoid division in the common cases 1 ns and 1 s. */
+	if (gran == 1) {
+		/* nothing */
+	} else if (gran == NSEC_PER_SEC) {
+		t.tv_nsec = 0;
+	} else if (gran > 1 && gran < NSEC_PER_SEC) {
+		t.tv_nsec -= t.tv_nsec % gran;
+	} else {
+		WARN(1, "illegal file time granularity: %u", gran);
+	}
+	return t;
+}
+EXPORT_SYMBOL(timespec64_trunc);
+
+/**
+ * current_time - Return FS time
+ * @inode: inode.
+ *
+ * Truncate a timespec64 to a granularity. Always rounds down. gran must
+ * not be 0 nor greater than a second (NSEC_PER_SEC, or 10^9 ns).
+ */
 struct timespec64 current_time(struct inode *inode)
 {
 	struct timespec64 now = current_kernel_time64();
