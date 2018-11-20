2034e863f64dfce675b91c7b8bcfe679924ac53c
Frank Luo <luofrank@google.com>
2018-11-20 15:33:34 +0800

    suspend: Workaround sensor ipc high power consume
    Sync from Qcom's document KBA-180725024109
    To avoid the non-wakeup type sensor data break the AP sleep flow,
    notify sensor subsystem in the first place of pm_suspend .
    
    Bug: 118418963
    Test: measure power consumption after running test case
    Change-Id: I2848230d495e30ac462aef148b3f885103d9c24e
    Signed-off-by: Frank Luo <luofrank@google.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -33,8 +33,13 @@
 #include <linux/compiler.h>
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
+#include <linux/soc/qcom/smem_state.h>
 #include "power.h"
 #include <soc/qcom/boot_stats.h>
+
+#define PROC_AWAKE_ID 12 /* 12th bit */
+#define AWAKE_BIT BIT(PROC_AWAKE_ID)
+extern struct qcom_smem_state *smem_state;
 
 const char * const pm_labels[] = {
 	[PM_SUSPEND_TO_IDLE] = "freeze",
@@ -632,10 +637,10 @@ int pm_suspend(suspend_state_t state)
 
 	if (state <= PM_SUSPEND_ON || state >= PM_SUSPEND_MAX)
 		return -EINVAL;
-
-	pm_suspend_marker("entry");
-	pr_info("suspend entry (%s)\n", mem_sleep_labels[state]);
+	pr_debug("suspend entry (%s)\n", mem_sleep_labels[state]);
+	qcom_smem_state_update_bits(smem_state, AWAKE_BIT, 0);
 	error = enter_state(state);
+	qcom_smem_state_update_bits(smem_state, AWAKE_BIT, AWAKE_BIT);
 	if (error) {
 		suspend_stats.fail++;
 		dpm_save_failed_errno(error);
