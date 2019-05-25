3af9b42a5be15270685d12f0c4b7bd5e3eef7d1f
Wei Wang <wvw@google.com>
2019-05-24 19:21:50 -0700

    Revert "sched/core: Fix use after free issue in is_sched_lib_based_app()"
    This reverts commit 0e6ca1640cec57004d702e5e7c3e59ba77541e2f.
    
    Bug:133481659
    Test: build
    Change-Id: Ie6a0b5e46386c98882614be19dedc61ffd3870e5
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5265,7 +5265,6 @@ static inline bool is_sched_lib_based_app(pid_t pid)
 	char path_buf[LIB_PATH_LENGTH];
 	bool found = false;
 	struct task_struct *p;
-	struct mm_struct *mm;
 	struct libname_node *pos;
 
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
@@ -5294,12 +5293,11 @@ static inline bool is_sched_lib_based_app(pid_t pid)
 	}
 	spin_unlock(&__sched_lib_name_lock);
 
-	mm = get_task_mm(p);
-	if (!mm)
+	if (!p->mm)
 		goto put_task_struct;
 
-	down_read(&mm->mmap_sem);
-	for (vma = mm->mmap; vma ; vma = vma->vm_next) {
+	down_read(&p->mm->mmap_sem);
+	for (vma = p->mm->mmap; vma ; vma = vma->vm_next) {
 		if (vma->vm_file && vma->vm_flags & VM_EXEC) {
 			name = d_path(&vma->vm_file->f_path,
 					path_buf, LIB_PATH_LENGTH);
@@ -5322,8 +5320,7 @@ static inline bool is_sched_lib_based_app(pid_t pid)
 	}
 
 release_sem:
-	up_read(&mm->mmap_sem);
-	mmput(mm);
+	up_read(&p->mm->mmap_sem);
 put_task_struct:
 	put_task_struct(p);
 	return found;
