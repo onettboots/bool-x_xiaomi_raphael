2315d409aa44bc1b56a60d84699540645eab1327
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-09-11 12:50:58 +0530

    sched/fair: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I7a84c7f7444d57f31e0859c4d39450d23421eb73
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7607,7 +7607,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		return -1;
 
 	/* fast path for prev_cpu */
-	if ((capacity_orig_of(prev_cpu) == capacity_orig_of(cpu)) &&
+	if (((capacity_orig_of(prev_cpu) == capacity_orig_of(cpu)) ||
+		asym_cap_siblings(prev_cpu, cpu)) &&
 		!cpu_isolated(prev_cpu) && cpu_online(prev_cpu) &&
 		idle_cpu(prev_cpu)) {
 
