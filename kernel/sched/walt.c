cff9e1e53cb402f3d15c651cfa7e086cc7e76e06
Sai Harshini Nimmala <snimmala@codeaurora.org>
2019-11-08 12:24:04 -0800

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I985879c5bf4951aeffaca1b43ea00f276e8c31d5
    Signed-off-by: Sai Harshini Nimmala <snimmala@codeaurora.org>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -128,7 +128,7 @@ static void release_rq_locks_irqrestore(const cpumask_t *cpus,
 /* 1 -> use PELT based load stats, 0 -> use window-based load stats */
 unsigned int __read_mostly walt_disabled = 0;
 
-__read_mostly unsigned int sysctl_sched_cpu_high_irqload = (10 * NSEC_PER_MSEC);
+__read_mostly unsigned int sysctl_sched_cpu_high_irqload = TICK_NSEC;
 
 unsigned int sysctl_sched_walt_rotate_big_tasks;
 unsigned int walt_rotation_enabled;
