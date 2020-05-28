a7abe891a38850267912a3b69d3ecd9a0ad58210
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-05-28 11:46:29 +0530

    sched/walt: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I5f19453ec1a19c71d67a02c3f0ec9967e7673a4b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7550,7 +7550,6 @@ enum fastpaths {
 	NONE = 0,
 	SYNC_WAKEUP,
 	PREV_CPU_FASTPATH,
-	MANY_WAKEUP,
 };
 
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
@@ -8204,8 +8203,14 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 #elif  CONFIG_UCLAMP_TASK
 	int boosted = (uclamp_boosted(p) > 0 || task_boost > 0);
 #endif
-    int start_cpu = get_start_cpu(p);
+	int start_cpu;
 
+	if (is_many_wakeup(sibling_count_hint) && prev_cpu != cpu &&
+			cpumask_test_cpu(prev_cpu, &p->cpus_allowed))
+		return prev_cpu;
+
+	start_cpu = get_start_cpu(p);
+	
 	if (start_cpu < 0)
 		goto eas_not_ready;
 
@@ -8227,12 +8232,6 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 		goto out;
 	}
 
-	if (is_many_wakeup(sibling_count_hint) && prev_cpu != cpu &&
-				bias_to_this_cpu(p, prev_cpu, start_cpu)) {
-		target_cpu = prev_cpu;
-		fbt_env.fastpath = MANY_WAKEUP;
-		goto out;
-	}
 
 	/* prepopulate energy diff environment */
 	eenv = get_eenv(p, prev_cpu);
