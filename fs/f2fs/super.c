a149f391acbf389537b4a61461834397b1a9a29e
Nick Terrell <terrelln@fb.com>
2020-09-11 16:49:00 -0700

    BACKPORT: lib: zstd: Add kernel-specific API
    This patch:
    - Moves `include/linux/zstd.h` -> `include/linux/zstd_lib.h`
    - Updates modified zstd headers to yearless copyright
    - Adds a new API in `include/linux/zstd.h` that is functionally
      equivalent to the in-use subset of the current API. Functions are
      renamed to avoid symbol collisions with zstd, to make it clear it is
      not the upstream zstd API, and to follow the kernel style guide.
    - Updates all callers to use the new API.
    
    There are no functional changes in this patch. Since there are no
    functional change, I felt it was okay to update all the callers in a
    single patch. Once the API is approved, the callers are mechanically
    changed.
    
    This patch is preparing for the 3rd patch in this series, which updates
    zstd to version 1.4.10. Since the upstream zstd API is no longer exposed
    to callers, the update can happen transparently.
    
    Signed-off-by: Nick Terrell <terrelln@fb.com>
    Tested By: Paul Jones <paul@pauljones.id.au>
    Tested-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com> # LLVM/Clang v13.0.0 on x86-64
    Tested-by: Jean-Denis Girard <jd.girard@sysnux.pf>
    [cyberknight777: backport to 4.14]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+k// SPDX-License-Identifier: GPL-2.0
 /*
  * fs/f2fs/super.c
  *
@@ -456,6 +456,73 @@ static int f2fs_set_test_dummy_encryption(struct super_block *sb,
 #endif
 	return 0;
 }
+
+#ifdef CONFIG_F2FS_FS_LZ4
+static int f2fs_set_lz4hc_level(struct f2fs_sb_info *sbi, const char *str)
+{
+#ifdef CONFIG_F2FS_FS_LZ4HC
+	unsigned int level;
+#endif
+
+	if (strlen(str) == 3) {
+		F2FS_OPTION(sbi).compress_level = 0;
+		return 0;
+	}
+
+#ifdef CONFIG_F2FS_FS_LZ4HC
+	str += 3;
+
+	if (str[0] != ':') {
+		f2fs_info(sbi, "wrong format, e.g. <alg_name>:<compr_level>");
+		return -EINVAL;
+	}
+	if (kstrtouint(str + 1, 10, &level))
+		return -EINVAL;
+
+	if (level < LZ4HC_MIN_CLEVEL || level > LZ4HC_MAX_CLEVEL) {
+		f2fs_info(sbi, "invalid lz4hc compress level: %d", level);
+		return -EINVAL;
+	}
+
+	F2FS_OPTION(sbi).compress_level = level;
+	return 0;
+#else
+	f2fs_info(sbi, "kernel doesn't support lz4hc compression");
+	return -EINVAL;
+#endif
+}
+#endif
+
+#ifdef CONFIG_F2FS_FS_ZSTD
+static int f2fs_set_zstd_level(struct f2fs_sb_info *sbi, const char *str)
+{
+	unsigned int level;
+	int len = 4;
+
+	if (strlen(str) == len) {
+		F2FS_OPTION(sbi).compress_level = 0;
+		return 0;
+	}
+
+	str += len;
+
+	if (str[0] != ':') {
+		f2fs_info(sbi, "wrong format, e.g. <alg_name>:<compr_level>");
+		return -EINVAL;
+	}
+	if (kstrtouint(str + 1, 10, &level))
+		return -EINVAL;
+
+	if (!level || level > zstd_max_clevel()) {
+		f2fs_info(sbi, "invalid zstd compress level: %d", level);
+		return -EINVAL;
+	}
+
+	F2FS_OPTION(sbi).compress_level = level;
+	return 0;
+}
+#endif
+#endif
 
 static int parse_options(struct super_block *sb, char *options, bool is_remount)
 {
