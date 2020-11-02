f58166180fd9d359e7026e5468eee2c2fc73d043
Vinayak Menon <vinmenon@codeaurora.org>
2020-11-02 06:39:22 -0800

    ANDROID: zram: allow zram to allocate CMA pages
    Though zram pages are movable, they aren't allowed to enter
    MIGRATE_CMA pageblocks. zram is not seen to pin pages for
    long which can cause an issue. Moreover allowing zram to
    pick CMA pages can be helpful in cases seen where zram order
    0 alloc fails when there are lots of free cma pages, resulting
    in kswapd or direct reclaim not making enough progress.
    
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>
    
    Bug: 158645321
    Link: https://lore.kernel.org/linux-mm/4c77bb100706b714213ff840d827a48e40ac9177.1604282969.git.cgoldswo@codeaurora.org/
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    [isaacm@codeaurora.org: Resolve trivial merge conflicts]
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    Change-Id: I406f92a4175367caec38ef8b8eaca7020ae09917
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -363,7 +363,7 @@ static void cache_free_handle(struct zs_pool *pool, unsigned long handle)
 static struct zspage *cache_alloc_zspage(struct zs_pool *pool, gfp_t flags)
 {
 	return kmem_cache_zalloc(pool->zspage_cachep,
-			flags & ~(__GFP_HIGHMEM|__GFP_MOVABLE));
+			flags & ~(__GFP_HIGHMEM|__GFP_MOVABLE|__GFP_CMA));
 }
 
 static void cache_free_zspage(struct zs_pool *pool, struct zspage *zspage)
