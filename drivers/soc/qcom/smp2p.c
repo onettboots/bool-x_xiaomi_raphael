075381b01b10266b69d1dbd3a04d5e4799bc9bb1
Chris Lew <clew@codeaurora.org>
2020-11-06 14:37:02 -0800

    soc: qcom: smp2p: Add memory barrier for irq_pending
    There is a very tight race where the irq_retrigger function is run
    on one cpu and the actual retrigger softirq is running on a second
    cpu. When this happens, there may be a chance that the second cpu
    will not see the updated irq_pending value from first cpu.
    
    Add a memory barrier to ensure that irq_pending is read correctly.
    
    Change-Id: I3dd185decc4f050bd57c0b6558f417ead2a3aa5a
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -274,6 +274,9 @@ static void qcom_smp2p_notify_in(struct qcom_smp2p *smp2p)
 
 		status = val ^ entry->last_value;
 		entry->last_value = val;
+
+		/* Ensure irq_pending is read correctly */
+		mb();
 		status |= *entry->irq_pending;
 
 		/* No changes of this entry? */
@@ -387,6 +390,22 @@ static int smp2p_set_irq_type(struct irq_data *irqd, unsigned int type)
 		set_bit(irq, entry->irq_falling);
 	else
 		clear_bit(irq, entry->irq_falling);
+
+	return 0;
+}
+
+static int smp2p_retrigger_irq(struct irq_data *irqd)
+{
+	struct smp2p_entry *entry = irq_data_get_irq_chip_data(irqd);
+	irq_hw_number_t irq = irqd_to_hwirq(irqd);
+
+	SMP2P_INFO("%d: %s: %lu\n", entry->smp2p->remote_pid, entry->name, irq);
+	set_bit(irq, entry->irq_pending);
+
+	/* Ensure irq_pending is visible to all cpus that retried interrupt
+	 * can run on
+	 */
+	mb();
 
 	return 0;
 }
