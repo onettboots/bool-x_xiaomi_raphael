3896f716c4f5ae78b3f58873e6bd04280dbd0c7b
Peter Zijlstra <peterz@infradead.org>
2021-05-13 01:29:23 +0200

    tick/nohz: Call tick_nohz_task_switch() with interrupts disabled
    Call tick_nohz_task_switch() slightly earlier after the context switch
    to benefit from disabled IRQs. This way the function doesn't need to
    disable them once more.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20210512232924.150322-10-frederic@kernel.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2942,6 +2942,7 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	 * to use.
 	 */
 	smp_mb__after_unlock_lock();
+	tick_nohz_task_switch();
 	finish_lock_switch(rq, prev);
 	finish_arch_post_lock_switch();
 	kcov_finish_switch(current);
@@ -2964,7 +2965,6 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 			finish_task_switch_dead(prev);
 	}
 
-	tick_nohz_task_switch();
 	return rq;
 }
 
