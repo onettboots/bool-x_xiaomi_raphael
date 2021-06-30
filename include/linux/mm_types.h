5bb743e6ac3b88db270ee59156a6f3216986d4f5
Sultan Alsawaf <sultan@kerneltoast.com>
2021-06-30 17:13:16 +0530

    mm: Speed up PID map reads for Android's use case
    Android frequently reads /proc/self/maps inside Bionic in order to
    determine the size of the current process' stack when pthreads are used.
    When the current process has several thousands of mappings though, such
    as for a game, a lot of time is wasted printing out every mapping when
    Bionic only cares about finding the stack mapping. We can optimize PID
    map reads for Android by caching the VMA that has the process' stack and
    only printing out the stack mapping every time the maps are read. This
    not only massively speeds up map reads for large processes, but also
    reduces the amount of time the mmap semaphore is held, which can provide other
    potential benefits when there's a writer waiting.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -371,8 +371,10 @@ struct core_state {
 
 struct kioctx_table;
 struct mm_struct {
-	struct vm_area_struct *mmap;		/* list of VMAs */
-	struct rb_root mm_rb;
+	struct {
+		struct vm_area_struct *mmap *stack_vma;		/* list of VMAs */
+		struct rb_root mm_rb;
+		u64 vmacache_seqnum;                   /* per-thread vmacache */
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
 	rwlock_t mm_rb_lock;
 #endif
