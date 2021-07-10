c1e583406c0bf1faa68a445fa642ec290f3a7b9e
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-09 21:46:09 -0700

    mm: vmpressure: Fix a race that would erroneously clear accumulated data
    Since the code that determines whether data should be cleared and the
    code that actually clears the data are in separate spin-locked critical
    sections, new data could be generated on another CPU after it is
    determined that the existing data should be cleared, but before the
    current CPU clears the existing data. This would cause the new data
    reported by the other CPU to be lost.
    
    Fix the race by clearing accumulated data within the same spin-locked
    critical section that determines whether or not data should be cleared.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -381,28 +381,24 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	if (!(gfp & (__GFP_HIGHMEM | __GFP_MOVABLE | __GFP_IO | __GFP_FS)))
 		return;
 
-	if (!scanned)
-		return;
+	spin_lock(&vmpr->sr_lock);
+	if (scanned) {
+		vmpr->scanned += scanned;
+		vmpr->reclaimed += reclaimed;
 
 	spin_lock(&vmpr->sr_lock);
 	if (!vmpr->scanned)
 		calculate_vmpressure_win();
 
-	vmpr->scanned += scanned;
-	vmpr->reclaimed += reclaimed;
+		stall = vmpr->stall;
+		scanned = vmpr->scanned;
+		reclaimed = vmpr->reclaimed;
 
-	if (!current_is_kswapd())
-		vmpr->stall += scanned;
-
-	stall = vmpr->stall;
-	scanned = vmpr->scanned;
-	reclaimed = vmpr->reclaimed;
-	spin_unlock(&vmpr->sr_lock);
-
-	if (scanned < vmpressure_win)
-		return;
-
-	spin_lock(&vmpr->sr_lock);
+		if (!critical && scanned < calculate_vmpressure_win()) {
+			spin_unlock(&vmpr->sr_lock);
+			return;
+		}
+	}
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
 	vmpr->stall = 0;
