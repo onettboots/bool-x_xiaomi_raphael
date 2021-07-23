cd697da1c50f7f59e5abd939e603aa6e7f9319b8
Danny Lin <danny@kdrag0n.dev>
2021-07-22 22:21:34 -0400

    drm/drm_atomic: Fallback to boosting devfreq within 3s of last interaction
    For power savings.
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2265,9 +2265,11 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+		if (time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
+			cpu_input_boost_kick();
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+			devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+		}
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
