7582dfaec1a0509e2b815efb3a0b85c41555f4ad
Sultan Alsawaf <sultan@kerneltoast.com>
2021-08-10 20:30:58 -0700

    simple_lmk: Be extra paranoid if tasks can have no pages
    If it's possible for a task to have no pages, then there could be a case
    where `pages_found` is zero while `nr_found` isn't, which would cause
    the found tasks' locks to never be unlocked, and thus mayhem. We can
    change the `pages_found` check to use `nr_found` instead in order to
    naturally defend against this scenario, in case it is indeed possible.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -192,9 +192,8 @@ static void scan_and_kill(void)
 
 	/* Populate the victims array with tasks sorted by adj and then size */
 	pages_found = find_victims(&nr_found);
-
-	if (unlikely(!pages_found)) {
-		printk_once("No processes available to kill!\n");
+	if (unlikely(!nr_found)) {
+		pr_err("No processes available to kill!\n");
 		return;
 	}
 
