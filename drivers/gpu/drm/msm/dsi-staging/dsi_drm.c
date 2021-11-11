cc1e473ec5f5a126134f0dafe23994b8187059bf
Raghavendra Ambadas <rambad@codeaurora.org>
2021-11-11 11:33:49 +0530

    drm/msm: Avoid dynamic mode switch during first commit
    Dynamic mode switch (DMS) is not supported for video mode panels before cont-splash handoff handled for first frame. so avoid dynamic mode-switch during cont-splash handoff for any DRM mode change.
    
    WA is given by QC for GSI issue, as of observation there are no
    side effects
    
    QC SR:05515278
    QC CR:NA
    QC Change ID:Icd5881af99afb3e398d3bba3746b7a35bcda4491
    
    Change-Id: I97f5712ce5bb8448f1c600ccf306d0dac7fa6eae
    Signed-off-by: maheshmk <maheshmk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2113033
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ashwin Kumar Pathmudi <jfxr63@motorola.com>
    Reviewed-by: Shuo Yan <shuoyan@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -475,7 +475,13 @@ static bool dsi_bridge_mode_fixup(struct drm_bridge *bridge,
 			(!(dsi_mode.dsi_mode_flags & DSI_MODE_FLAG_DYN_CLK)) &&
 			(!crtc_state->active_changed ||
 			 display->is_cont_splash_enabled))
-			dsi_mode.dsi_mode_flags |= DSI_MODE_FLAG_DMS;
+			/* Reject DMS for First commit for Video Mode Panel */
+			if((dsi_mode.panel_mode == DSI_OP_VIDEO_MODE) &&
+							display->is_cont_splash_enabled)
+				pr_err("DMS not supported for display type:%d for first frame\n",
+							dsi_mode.panel_mode);
+			else
+				dsi_mode.dsi_mode_flags |= DSI_MODE_FLAG_DMS;
 
 		/* Reject seemless transition when active/connectors changed.*/
 		if ((crtc_state->active_changed ||
