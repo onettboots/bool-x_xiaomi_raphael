afd4c030db0f011eada27e51ff3bb8df4a6c7434
Cyber Knight <cyberknight755@gmail.com>
2021-12-20 01:49:38 +0800

    kernel/cpu: Silence abundance of logspam
    We don't really need to know if the CPU is getting disabled or enabled on a production device.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1341,7 +1341,7 @@ int freeze_secondary_cpus(int primary)
 			continue;
 
 		if (pm_wakeup_pending()) {
-			pr_info("Wakeup pending. Abort CPU%i freeze\n", cpu);
+			pr_debug("Wakeup pending. Abort CPU%i freeze\n", cpu);
 			error = -EBUSY;
 			break;
 		}
