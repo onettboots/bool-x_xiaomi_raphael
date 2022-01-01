ae4d91bdbd01ccd5d8f8fc8f0ba467c0d08cb2e1
Andrzej Perczak <linux@andrzejperczak.com>
2022-01-01 17:58:01 +0100

    fs: Add __close_fd_get_file() for binder
    Change-Id: I9fe7c84d1b81fd8e2ef0ad89361c615c62693593
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/fdtable.h
+++ include/linux/fdtable.h
@@ -126,6 +126,8 @@ extern int __close_fd(struct files_struct *files,
 		      unsigned int fd);
 extern int __close_range(unsigned int fd, unsigned int max_fd, unsigned int flags);
 
+extern int __close_fd_get_file(unsigned int fd, struct file **res);
+
 extern struct kmem_cache *files_cachep;
 
 #endif /* __LINUX_FDTABLE_H */
