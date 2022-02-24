0bf5fcd2f22e5aa92bb115d5e5d4d9dc233bcb52
RuRuTiaSaMa <1009087450@qq.com>
2022-02-25 02:42:14 +0800

    techpack:audio:update drivers from nabu-r-oss
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- techpack/audio/asoc/codecs/tfa98xx/inc/config.h
+++ techpack/audio/asoc/codecs/tfa98xx/inc/config.h
@@ -1,6 +1,5 @@
 /*
  * Copyright 2014-2017 NXP Semiconductors
- * Copyright (C) 2019 XiaoMi, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
