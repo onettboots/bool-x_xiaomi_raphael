9cad0163fc03f7fff73bcd8b3d245535a8188149
Sultan Alsawaf <sultan@kerneltoast.com>
2022-03-03 21:36:03 -0800

    cpuidle: lpm-levels: Convert sync_lock to raw spin lock
    The CPU idle loop runs with IRQs disabled, so a raw spin lock must be used
    here.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I7bd697b66eb2d3c8642f46c306832716b54f8325
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/cpuidle/lpm-levels.h
+++ drivers/cpuidle/lpm-levels.h
@@ -78,7 +78,7 @@ struct lpm_cluster {
 	int default_level;
 	int last_level;
 	struct list_head cpu;
-	spinlock_t sync_lock;
+	raw_spinlock_t sync_lock;
 	struct cpumask child_cpus;
 	struct cpumask num_children_in_sync;
 	struct lpm_cluster *parent;
