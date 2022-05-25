bfa49055e9c9df412e47591c8e7f40a68e5f54c4
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-05-25 11:07:49 +0800

    Revert "sysctl: Nuke sched_boost feature"
    This reverts commit 425719f6ad8ea56ca80cab9ce671734223b545eb.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -393,7 +393,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
-#if 0
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
@@ -403,7 +402,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &neg_three,
 		.extra2		= &three,
 	},
-#endif
 	{
 		.procname	= "sched_many_wakeup_threshold",
 		.data		= &sysctl_sched_many_wakeup_threshold,
