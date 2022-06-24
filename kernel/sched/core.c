aede1b2f2262ab1a32d794cd2d76ea71856f9af2
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-06-24 15:55:50 +0800

    Revert "psi: Fix cpu.pressure for cpu.max and competing cgroups"
    This reverts commit 3a39881d58928f6ae26686b4a8b5268818a3efda.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3721,8 +3721,6 @@ static void __sched notrace __schedule(bool preempt)
 		 */
 		++*switch_count;
 
-		psi_sched_switch(prev, next, !task_on_rq_queued(prev));
-
 		trace_sched_switch(preempt, prev, next);
 
 		/* Also unlocks the rq: */
