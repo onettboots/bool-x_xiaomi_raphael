4262ba307facaad3f866d8431b7d95d605330d8e
John Galt <johngaltfirstrun@gmail.com>
2022-07-15 09:13:14 -0400

    [kprofiles] cpu_input_boost: set min freq on all but battery
    In many usages people would avoid using battery mode on, this actually
    improves efficiency. So keep it for all.
    
    Amends d997e6dc614b5989e22c4aaa66ae2624a20b0cb5

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -72,6 +72,39 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	return min(freq, policy->max);
 }
 
+extern int kp_active_mode(void);
+static unsigned int get_min_freq(struct cpufreq_policy *policy)
+{
+	unsigned int freq;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = cpu_freq_min_little;
+
+	if (kp_active_mode() != 1) {
+		if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+			freq = cpu_freq_min_big;
+		else if (cpumask_test_cpu(policy->cpu, cpu_prime_mask))
+			freq = cpu_freq_min_prime;
+	}
+
+	return max(freq, policy->cpuinfo.min_freq);
+}
+
+static unsigned int get_idle_freq(struct cpufreq_policy *policy)
+{
+	unsigned int freq;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = cpu_freq_idle_little;
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+		freq = CONFIG_CPU_FREQ_IDLE_PERF;
+	else
+		freq = CONFIG_CPU_FREQ_IDLE_PERFP;
+
+	return max(freq, policy->cpuinfo.min_freq);
+}
+
+
 static void update_online_cpu_policy(void)
 {
 	unsigned int cpu;
