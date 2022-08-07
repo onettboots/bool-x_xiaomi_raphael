39bb144ef94738c7b43cdbe0d1bc19ecd803dd0b
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-08-07 12:48:09 +0800

    Revert "sched: optimize for PELT"
    This reverts commit a2c82a6a5da45e3ca30f2b4ba3e3c7b0689e52a2.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -248,7 +248,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 
 		policy->cur = next_freq;
 	} else {
-		if (likely(use_pelt()))
+		if (use_pelt())
 			sg_policy->work_in_progress = true;
 		irq_work_queue(&sg_policy->irq_work);
 	}
@@ -586,7 +586,7 @@ static void sugov_work(struct kthread_work *work)
 				CPUFREQ_RELATION_L);
 	mutex_unlock(&sg_policy->work_lock);
 
-	if (likely(use_pelt()))
+	if (use_pelt())
 		sg_policy->work_in_progress = false;
 }
 
