d4827246bb3fa0cf7c65a6a3761e1bbb9a6fb4a4
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-08-28 13:10:19 +0800

    Revert "locking/rtmutex: Handle non enqueued waiters gracefully in remove_waiter()"
    This reverts commit a9d111abd8448b65736d4ec9e8898b3de1edd15f.

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -1268,7 +1268,8 @@ rt_mutex_slowlock(struct rt_mutex *lock, int state,
 
 	if (unlikely(ret)) {
 		__set_current_state(TASK_RUNNING);
-		remove_waiter(lock, &waiter);
+		if (rt_mutex_has_waiters(lock))
+			remove_waiter(lock, &waiter);
 		rt_mutex_handle_deadlock(ret, chwalk, &waiter);
 	}
 
