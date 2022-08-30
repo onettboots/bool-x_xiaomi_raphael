RealJohnGalt/GaltsGulch-sm8150

Fod/goodix irq: wakeup and suspend optimization

drivers/input/fingerprint/goodix/gf_spi.c
@@ -265,7 +265,7 @@ static inline int irq_setup(struct gf_dev *gf_dev)
 	int status;
 	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_HP_AFFINE,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT |IRQF_NO_SUSPEND | IRQF_FORCE_RESUME | IRQF_HP_AFFINE,
 			"gf", gf_dev);
 
 	if (status) {

onettboots/bool-x_xiaomi_raphael

drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -565,8 +565,7 @@ static int gf_open(struct inode *inode, struct file *filp)
 			gpio_direction_input(gf_dev->irq_gpio);
 
 			rc = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-					IRQF_TRIGGER_RISING |
-					IRQF_ONESHOT | IRQF_PRIME_AFFINE,
+					IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_NO_SUSPEND | IRQF_FORCE_RESUME | IRQF_PRIME_AFFINE,
 					"gf", gf_dev);
 			if (!rc) {
 				enable_irq_wake(gf_dev->irq);