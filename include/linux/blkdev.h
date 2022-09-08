9e014339b36ce84397956f620ebe702bc81ede21
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-09-08 10:41:48 +0800

    [SQUASH] block: revert previous reverts
    I don't think we're affected by this?
    
    Revert "Revert "block: Make q_usage_counter also track legacy requests" [7/7]"
    
    This reverts commit 223a9d5ae03ea2487e9d41d16049114ede3db131.
    
    Revert "Revert "block: Convert RQF_PREEMPT into REQ_PREEMPT" [6/7]"
    
    This reverts commit 8a1438b95486894c2e2d8e401862545feaae390e.
    
    Revert "Revert "block: pass 'op' to blk_queue_enter()" [5/7]"
    
    This reverts commit a838f088b59c4dad9e089d8fb3cb1386aaf89da5.
    
    Revert "Revert "percpu-refcount: introduce __percpu_ref_tryget_live" [4/7]"
    
    This reverts commit 1f27e71632ca1135151deb137d2a2cf1ef2b37bc.
    
    Revert "Revert "blk-mq: return if queue is frozen via current blk_freeze_queue_start" [3/7]"
    
    This reverts commit a37e57d7fbe55ba371b72b761c835da93d64ad2b.
    
    Revert "Revert "block: support PREEMPT_ONLY""
    
    This reverts commit 275b22e4117acfb911c7b671091f7bff5424df06.
    
    Revert "Revert "SCSI: set block queue at preempt only when SCSI device is put into quiesce" [1/7]"
    
    This reverts commit b3faff519b4f0301d8d014bf95fe4fda8b8878d3.

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -108,9 +108,6 @@ typedef __u32 __bitwise req_flags_t;
 #define RQF_MQ_INFLIGHT		((__force req_flags_t)(1 << 6))
 /* don't call prep for this one */
 #define RQF_DONTPREP		((__force req_flags_t)(1 << 7))
-/* set for "ide_preempt" requests and also for requests for which the SCSI
-   "quiesce" state must be ignored. */
-#define RQF_PREEMPT		((__force req_flags_t)(1 << 8))
 /* contains copies of user pages */
 #define RQF_COPY_USER		((__force req_flags_t)(1 << 9))
 /* vaguely specified driver internal error.  Ignored by the block layer */
@@ -704,6 +701,7 @@ struct request_queue {
 #define QUEUE_FLAG_REGISTERED  26	/* queue has been registered to a disk */
 #define QUEUE_FLAG_SCSI_PASSTHROUGH 27	/* queue supports SCSI commands */
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
+#define QUEUE_FLAG_PREEMPT_ONLY 29	/* only process REQ_PREEMPT requests */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_NONROT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
@@ -813,6 +811,10 @@ static inline void queue_flag_clear(unsigned int flag, struct request_queue *q)
 			     REQ_FAILFAST_DRIVER))
 #define blk_queue_quiesced(q)	test_bit(QUEUE_FLAG_QUIESCED, &(q)->queue_flags)
 #define blk_queue_registered(q)	test_bit(QUEUE_FLAG_REGISTERED, &(q)->queue_flags)
+#define blk_queue_preempt_only(q)				\
+	test_bit(QUEUE_FLAG_PREEMPT_ONLY, &(q)->queue_flags)
+
+extern void blk_set_preempt_only(struct request_queue *q, bool preempt_only);
 
 static inline bool blk_account_rq(struct request *rq)
 {
@@ -1045,7 +1047,7 @@ extern int scsi_cmd_ioctl(struct request_queue *, struct gendisk *, fmode_t,
 extern int sg_scsi_ioctl(struct request_queue *, struct gendisk *, fmode_t,
 			 struct scsi_ioctl_command __user *);
 
-extern int blk_queue_enter(struct request_queue *q, bool nowait);
+extern int blk_queue_enter(struct request_queue *q, unsigned int op);
 extern void blk_queue_exit(struct request_queue *q);
 extern void blk_start_queue(struct request_queue *q);
 extern void blk_start_queue_async(struct request_queue *q);
