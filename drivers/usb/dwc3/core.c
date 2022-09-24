6d0a36e36b60fc53536e897c92026b37a984f22a
Cyber Knight <cyberknight755@gmail.com>
2022-09-24 21:46:05 +0000

    dwc3/core: Fix logspam with !IPC_LOGGING and !DEBUG_FS Silences: [    0.722095] usbnet_init: Error getting ipc_log_ctxt [    0.722101] usbnet_init: Error getting ipc_log_ctxt [    0.722104] usbnet_init: Error getting ipc_log_ctxt [    0.730743] dwc3 a600000.dwc3: Error getting ipc_log_ctxt [    0.730765] dwc3 a600000.dwc3: Error getting ipc_log_ctxt for ep_events
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1359,7 +1359,9 @@ static int dwc3_probe(struct platform_device *pdev)
 
 	void __iomem		*regs;
 	int			irq;
+#ifdef CONFIG_IPC_LOGGING
 	char			dma_ipc_log_ctx_name[40];
+#endif
 
 	if (count >= DWC_CTRL_COUNT) {
 		dev_err(dev, "Err dwc instance %d >= %d available\n",
@@ -1458,6 +1460,7 @@ static int dwc3_probe(struct platform_device *pdev)
 		}
 	}
 
+#ifdef CONFIG_IPC_LOGGING
 	dwc->dwc_ipc_log_ctxt = ipc_log_context_create(NUM_LOG_PAGES,
 					dev_name(dwc->dev), 0);
 	if (!dwc->dwc_ipc_log_ctxt)
@@ -1470,12 +1473,16 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (!dwc->dwc_dma_ipc_log_ctxt)
 		dev_err(dwc->dev, "Error getting ipc_log_ctxt for ep_events\n");
 
+#endif
+
 	dwc3_instance[count] = dwc;
 	dwc->index = count;
 	count++;
 
 	pm_runtime_allow(dev);
+#ifdef CONFIG_DEBUG_FS
 	dwc3_debugfs_init(dwc);
+#endif
 	return 0;
 
 err5:
@@ -1526,7 +1533,9 @@ static int dwc3_remove(struct platform_device *pdev)
 	 */
 	res->start -= DWC3_GLOBALS_REGS_START;
 
+#ifdef CONFIG_DEBUG_FS
 	dwc3_debugfs_exit(dwc);
+#endif
 	pm_runtime_allow(&pdev->dev);
 	dwc3_core_exit_mode(dwc);
 
@@ -1538,9 +1547,10 @@ static int dwc3_remove(struct platform_device *pdev)
 
 	dwc3_free_event_buffers(dwc);
 	dwc3_free_scratch_buffers(dwc);
-
+#ifdef CONFIG_DEBUG_FS
 	ipc_log_context_destroy(dwc->dwc_ipc_log_ctxt);
 	dwc->dwc_ipc_log_ctxt = NULL;
+#endif
 	count--;
 	dwc3_instance[dwc->index] = NULL;
 
