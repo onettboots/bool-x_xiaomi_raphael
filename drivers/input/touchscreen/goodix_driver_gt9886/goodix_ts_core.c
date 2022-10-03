CowiiiTD/kernel_xiaomi_raphael

goodix_driver_gt9886: check aod_status for single tap in AOD feature
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
@@ -1193,7 +1193,7 @@ static void goodix_switch_mode_work(struct work_struct *work)
 	if (value >= INPUT_EVENT_WAKUP_MODE_OFF
 		&& value <= INPUT_EVENT_WAKUP_MODE_ON) {
 		info->double_wakeup = value - INPUT_EVENT_WAKUP_MODE_OFF;
-		info->gesture_enabled = info->double_wakeup | info->fod_status;
+		info->gesture_enabled = info->double_wakeup || info->aod_status;
 		/*goodix_gesture_enable(!!info->gesture_enabled);*/
 #ifdef CONFIG_GOODIX_HWINFO
 		snprintf(ch, sizeof(ch), "%s", info->gesture_enabled ? "enabled" : "disabled");
@@ -1550,11 +1550,11 @@ int goodix_ts_suspend(struct goodix_ts_core *core_data)
 
 			r = ext_module->funcs->before_suspend(core_data, ext_module);
 			if (r == EVT_CANCEL_SUSPEND) {
-				if (core_data->double_wakeup && (core_data->fod_enabled) {
+				if (core_data->double_wakeup && (core_data->aod_status || core_data->fod_enabled)) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_DBCLK_FOD);
-				} else if (core_data->double_wakeup && !core_data->fod_enabled) {
+				} else if (core_data->double_wakeup && (!core_data->aod_status && !core_data->fod_enabled)) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_DBCLK);
-				} else if (!core_data->double_wakeup && (core_data->fod_enabled) {
+				} else if (!core_data->double_wakeup && (core_data->aod_status || core_data->fod_enabled)) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_FOD);
 				}
 				mutex_unlock(&goodix_modules.mutex);
@@ -2248,7 +2248,6 @@ static int goodix_ts_probe(struct platform_device *pdev)
 		ts_err("Failed to create fod_test sysfs group!");
 		goto out;
 	}
-
 
 #ifdef CONFIG_GOODIX_HWINFO
 	core_data->dbclick_count = 0;

drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_gesture.c
@@ -551,25 +551,25 @@ static int goodix_set_suspend_func(struct goodix_ts_core *core_data)
 	u8 state_data[3] = {0};
 	int ret;
 
-	if (core_data->double_wakeup && (core_data->fod_enabled) {
+	if (core_data->double_wakeup && (core_data->aod_status || core_data->fod_enabled)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x01;
 		state_data[2] = 0xF7;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode on,FOD mode on;");
-	} else if (core_data->double_wakeup && (!core_data->fod_enabled) {
+	} else if (core_data->double_wakeup && (core_data->aod_status == 0 && !core_data->fod_enabled)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x03;
 		state_data[2] = 0xF5;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode on,FOD mode off;");
-	} else if (!core_data->double_wakeup && (core_data->fod_enabled) {
+	} else if (!core_data->double_wakeup && (core_data->aod_status || core_data->fod_enabled)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x00;
 		state_data[2] = 0xF8;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode off,FOD mode on;");
-	} else if (!core_data->double_wakeup && (!core_data->fod_enabled) {
+	} else if (!core_data->double_wakeup && (core_data->aod_status == 0 && !core_data->fod_enabled)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x02;
 		state_data[2] = 0xF6;

onettboots/bool-x_xiaomi_raphael

drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
@@ -1346,7 +1346,7 @@ static void goodix_switch_mode_work(struct work_struct *work)
 	if (value >= INPUT_EVENT_WAKUP_MODE_OFF
 		&& value <= INPUT_EVENT_WAKUP_MODE_ON) {
 		info->double_wakeup = value - INPUT_EVENT_WAKUP_MODE_OFF;
-		info->gesture_enabled = info->double_wakeup | info->fod_status;
+		info->gesture_enabled = info->double_wakeup || info->aod_status;
 		/*goodix_gesture_enable(!!info->gesture_enabled);*/
 #ifdef CONFIG_GOODIX_HWINFO
 		snprintf(ch, sizeof(ch), "%s", info->gesture_enabled ? "enabled" : "disabled");
@@ -1703,11 +1703,11 @@ int goodix_ts_suspend(struct goodix_ts_core *core_data)
 
 			r = ext_module->funcs->before_suspend(core_data, ext_module);
 			if (r == EVT_CANCEL_SUSPEND) {
-				if (core_data->double_wakeup && core_data->fod_status) {
+				if (core_data->double_wakeup && (core_data->aod_status || core_data->fod_status)) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_DBCLK_FOD);
-				} else if (core_data->double_wakeup) {
+				} else if (core_data->double_wakeup && (!core_data->aod_status)) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_DBCLK);
-				} else if (core_data->fod_status) {
+				} else if (core_data->fod_status && core_data->aod_status) {
 					atomic_set(&core_data->suspend_stat, TP_GESTURE_FOD);
 				}
 				mutex_unlock(&goodix_modules.mutex);

drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_gesture.c
@@ -551,25 +551,25 @@ static int goodix_set_suspend_func(struct goodix_ts_core *core_data)
 	u8 state_data[3] = {0};
 	int ret;
 
-	if (core_data->double_wakeup && core_data->fod_status) {
+	if (core_data->double_wakeup && (core_data->aod_status || core_data->fod_status)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x01;
 		state_data[2] = 0xF7;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode on,FOD mode on;");
-	} else if (core_data->double_wakeup && (!core_data->fod_status)) {
+	} else if (core_data->double_wakeup && (core_data->aod_status == 0 && !core_data->fod_status)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x03;
 		state_data[2] = 0xF5;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode on,FOD mode off;");
-	} else if (!core_data->double_wakeup && core_data->fod_status) {
+	} else if (!core_data->double_wakeup && (core_data->aod_status || core_data->fod_status)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x00;
 		state_data[2] = 0xF8;
 		ret = goodix_i2c_write(dev, GSX_REG_GESTURE, state_data, 3);
 		ts_info("Set IC double wakeup mode off,FOD mode on;");
-	} else if (!core_data->double_wakeup && (!core_data->fod_status)) {
+	} else if (!core_data->double_wakeup && (core_data->aod_status == 0 && !core_data->fod_status)) {
 		state_data[0] = GSX_GESTURE_CMD;
 		state_data[1] = 0x02;
 		state_data[2] = 0xF6;