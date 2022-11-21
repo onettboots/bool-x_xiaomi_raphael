a0a6d5547534125e052642f8a6c2e22bfc945caf
onettboots <blackcocopet@gmail.com>
2022-11-21 04:33:33 +0000

    [kprofiles]: add double boost on performance profiles
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -239,6 +239,7 @@ int devfreq_add_devbw(struct device *dev)
 	}
 
 	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw"))
+		devfreq_register_boost_device(DEVFREQ_CPU_LLCC_DDR_BW, d->df);
 		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
 
 	return 0;
