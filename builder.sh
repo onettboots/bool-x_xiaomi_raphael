c40d70704fd24c1b614824072b65976ef0eec8bb
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-12-15 13:36:42 +0800

    builder: move to AOSP clang 16.0.2

--- builder.sh
+++ builder.sh
@@ -17,7 +17,7 @@ export ARCH="arm64"
 TARGET_IMAGE="Image.gz-dtb"
 
 # Toolchains
-CLANG_VERSION="clang-r475365"
+CLANG_VERSION="clang-r475365b"
 CLANG_LOC="/home/pwnrazr/dev-stuff/${CLANG_VERSION}"
 CLANG="${CLANG_LOC}/bin:$PATH"
 CT_BIN="${CLANG}/bin/"
