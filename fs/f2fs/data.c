b314ac7c148c33acdb1ed05c06a602dc39a5ca6c
Chao Yu <chao@kernel.org>
2022-12-15 14:05:06 +0800

    f2fs: avoid to check PG_error flag
    After below changes:
    commit 14db0b3c7b83 ("fscrypt: stop using PG_error to track error status")
    commit 98dc08bae678 ("fsverity: stop using PG_error to track error status")
    
    There is no place in f2fs we will set PG_error flag in page, let's remove
    other PG_error usage in f2fs, as a step towards freeing the PG_error flag
    for other uses.
    
    Cc: Eric Biggers <ebiggers@kernel.org>
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2801,7 +2801,6 @@ got_it:
 			goto out_writepage;
 
 		set_page_writeback(page);
-		ClearPageError(page);
 		f2fs_put_dnode(&dn);
 		if (fio->need_lock == LOCK_REQ)
 			f2fs_unlock_op(fio->sbi);
@@ -2837,7 +2836,6 @@ got_it:
 		goto out_writepage;
 
 	set_page_writeback(page);
-	ClearPageError(page);
 
 	if (fio->compr_blocks && fio->old_blkaddr == COMPRESS_ADDR)
 		f2fs_i_compr_blocks_update(inode, fio->compr_blocks - 1, false);
