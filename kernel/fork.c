1e7b476341bf19290c11c5ce031aefd6ff5d6223
onettboots <blackcocopet@gmail.com>
2023-01-04 00:03:39 +0000

    [kprofiles]: update boost time
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2237,11 +2237,11 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max when userspace launches an app according to set kernel profile */
 	if (task_is_zygote(current) && (kp_active_mode() == 2)) {
-	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 75);
 	  cpu_input_boost_kick_max(50);
 	} else if (task_is_zygote(current) && ((kp_active_mode() == 3) || (kp_active_mode() == 0))) {
-	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 75);
-	  cpu_input_boost_kick_max(75);
+	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 700);
+	  cpu_input_boost_kick_max(500);
 	} else if (task_is_zygote(current) && (kp_active_mode() == 1)) {
 	  pr_info("Battery profile detected! Skipping DDR bus boost...\n");
 	}
