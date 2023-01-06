2d3115fcc0a9ad56c0a542ae1c20a920ea5d18fc
onettboots <blackcocopet@gmail.com>
2023-01-06 14:49:30 +0000

    cpuset: update for uclamp tune
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2133,13 +2133,12 @@ static void uclamp_set(struct kernfs_open_file *of,
 
 	static struct ucl_param tgts[] = {
 		{"top-app",    	     	"20", "max", 1, 1},  // 20-100%
-		{"foreground", 	     	"10", "80",  1, 0},  // 10-80%
-		{"background", 	     	"0",  "50",  0, 0},  // 0-50%
-		{"system-background", 	"0",  "60",  0, 0},  // 0-60%
+		{"foreground", 	     	"20", "70",  0, 0},  // 20-70%
+		{"background", 	     	"5",  "25",  0, 0},  // 5-25%
+		{"system-background", 	"0",  "20",  0, 0},  // 0-20%
+		{"camera-daemon",       "0",  "50",  1, 1},  // 0-50%
+		{"game",                "80", "max", 1, 1}, //80-100%
 		{"restricted",          "0",  "20",  0, 0},  // 0-20%
-		{"display",             "50", "100", 1, 1},
-		{"camera-daemon",       "20", "max", 1, 1},  // 20-100%
-		{"game",                "80", "max", 1, 1},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
