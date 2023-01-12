7f9db40b5ea96be7a4bc33daf797cf418bd03c46
John Galt <johngaltfirstrun@gmail.com>
2023-01-12 12:07:05 -0500

    Display: CSC DC dimming implementation
    FOD dim takes into account current dc dim alpha.
    
    Reads DC dimming lut from panel dtsi qcom,dc-dim-lut.
    
    Should be relatively generic and universal.
    
    If you want all layers to update on brightness changes at a
    performance cost rather than delayed, change dirty method.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -243,12 +243,16 @@ struct dsi_panel {
 	bool resend_ea_hbm;
 
 	struct brightness_alpha_pair *fod_dim_lut;
+	struct brightness_alpha_pair *dc_dim_lut;
 	unsigned int fod_dim_lut_len;
+	unsigned int dc_dim_lut_len;
 	u8 fod_dim_alpha;
+	u8 dc_dim_alpha;
 	bool fod_hbm_enabled;
 	bool fod_ui;
 	bool force_fod_ui;
 	bool force_fod_dim_alpha;
+	bool dc_dim;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -373,6 +377,7 @@ void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
 		struct dsi_display_mode *mode, u32 frame_threshold_us);
 
 u8 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
+u8 dsi_panel_get_dc_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
@@ -380,5 +385,6 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
 bool dsi_panel_get_force_fod_ui(struct dsi_panel *panel);
+bool dsi_panel_get_dc_dim(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
