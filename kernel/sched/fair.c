72d06f8c0b229767ea4af505fe2c7a50c3d09663
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:18:44 +0800

    Revert "sched/fair: Move avg_scan_cost calculations under SIS_PROP"
    This reverts commit e9756daca077a795edc428a818c49962b06c7925.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7666,8 +7666,6 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 	if (!this_sd)
 		return -1;
 
-	cpumask_and(cpus, sched_domain_span(sd), p->cpus_ptr);
-
 	if (sched_feat(SIS_PROP)) {
 		u64 avg_cost, avg_idle, span_avg;
 
@@ -7683,9 +7681,11 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 			nr = div_u64(span_avg, avg_cost);
 		else
 			nr = 4;
-
-		time = cpu_clock(this);
 	}
+
+	time = local_clock();
+
+	cpumask_and(cpus, sched_domain_span(sd), p->cpus_ptr);
 
 	for_each_cpu_wrap(cpu, cpus, target) {
 		if (!--nr)
@@ -7696,10 +7696,8 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 			break;
 	}
 
-	if (sched_feat(SIS_PROP)) {
-		time = local_clock() - time;
-		update_avg(&this_sd->avg_scan_cost, time);
-	}
+	time = local_clock() - time;
+	update_avg(&this_sd->avg_scan_cost, time);
 
 	return cpu;
 }
