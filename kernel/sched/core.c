35b9261a356812f8ee417592ef3a757994198c1f
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:53:47 +0800

    Revert "sched/core: Fix preempt warning in ttwu"
    This reverts commit 56848877ef14a889061df6fd9b1f95c29b667ed7.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2194,7 +2194,6 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags,
 	unsigned long flags;
 	int cpu, success = 0;
 
-	preempt_disable();
 	if (p == current) {
 		/*
 		 * We're waking current, this means 'p->on_rq' and 'task_cpu(p)
@@ -2208,7 +2207,7 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags,
 		 *    it disabling IRQs (this allows not taking ->pi_lock).
 		 */
 		if (!(p->state & state))
-			goto out;
+			return false;
 
 		success = 1;
 		cpu = task_cpu(p);
@@ -2336,7 +2335,6 @@ unlock:
 out:
 	if (success)
 		ttwu_stat(p, cpu, wake_flags);
-	preempt_enable();
 
 #ifdef CONFIG_SCHED_WALT
 	if (success && sched_predl) {
