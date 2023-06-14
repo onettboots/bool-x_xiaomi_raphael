b07cb1a667431b8c49abe8b0ffbbb0382557dc4f
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:57:31 +0800

    Revert "sched: Improve the scheduler"
    This reverts commit b184059b6590d4ca6f776bc760e5a625595cfc86.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7419,8 +7419,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (capacity == max_capacity)
 		return true;
 
-	if ((task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
-			schedtune_task_boost(p) > 0) &&
+	if (task_boost_policy(p) == SCHED_BOOST_ON_BIG &&
 			is_min_capacity_cpu(cpu))
 		return false;
 
