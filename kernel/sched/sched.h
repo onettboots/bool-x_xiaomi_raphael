9f6be2258966d8d402dae2528638bd6e95d7c0db
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:38:05 +0800

    Revert "sched/fair: Replace zero-length array with flexible-array"
    This reverts commit c00b423f1e5ab7dd1acbfb5a9689646de93c2756.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1449,7 +1449,7 @@ struct sched_group {
 	 * by attaching extra space to the end of the structure,
 	 * depending on how many CPUs the kernel has booted up with)
 	 */
-	unsigned long cpumask[];
+	unsigned long cpumask[0];
 };
 
 static inline struct cpumask *sched_group_span(struct sched_group *sg)
