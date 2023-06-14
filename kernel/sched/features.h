f1c5376bf79a87397d3b5120a9503d5c12f25da4
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:33:54 +0800

    Revert "sched/features: disable FBT_STRICT_ORDER"
    This reverts commit ebd340f848e1586a6072f6fe9948d0db9565b26c.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -126,7 +126,7 @@
  */
 #define SCHED_FEAT_EAS_PREFER_IDLE 1
 #define SCHED_FEAT_FIND_BEST_TARGET 1
-#define SCHED_FEAT_FBT_STRICT_ORDER 0
+#define SCHED_FEAT_FBT_STRICT_ORDER 1
 
 /*
  * Apply schedtune boost hold to tasks of all sched classes.
