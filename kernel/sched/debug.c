165603dace819842369f240434720388cff27a7f
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-14 12:23:48 +0800

    Revert "sched/debug: Make sysrq prints of sched debug data optional"
    This reverts commit 58c9c1ad83031c8fae81e6974288d8e1ef44fab1.

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -747,7 +747,6 @@ static int sched_debug_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-#ifdef CONFIG_SYSRQ_SCHED_DEBUG
 void sysrq_sched_debug_show(void)
 {
 	int cpu;
@@ -757,7 +756,6 @@ void sysrq_sched_debug_show(void)
 		print_cpu(NULL, cpu);
 
 }
-#endif
 
 /*
  * This itererator needs some explanation.
