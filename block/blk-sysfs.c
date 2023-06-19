5b9a5c3309bb2ff94210c00b1b56ae54bb0bb934
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-19 23:48:28 +0800

    Revert "blkdev: Do not perform IO stats"
    Prepare for next commits
    
    This reverts commit 5e20592ddd292f89f20afeb6caa9c482573b53ce.

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -633,7 +633,7 @@ static struct queue_sysfs_entry queue_rq_affinity_entry = {
 };
 
 static struct queue_sysfs_entry queue_iostats_entry = {
-	.attr = {.name = "iostats", .mode = S_IRUGO },
+	.attr = {.name = "iostats", .mode = S_IRUGO | S_IWUSR },
 	.show = queue_show_iostats,
 	.store = queue_store_iostats,
 };
