50a869cacad5009c9fb6c547fd3aa0855508d53d
John Galt <johngaltfirstrun@gmail.com>
2023-06-25 09:37:21 -0400

    devfreq_boost: only stop small kicks on kp 1
    Max boosts are still only allowed on kp 3. Benefits jank considerably to
    allow small kicks

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -62,7 +62,7 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 extern int kp_active_mode(void);
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() != 3)
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() == 1)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
