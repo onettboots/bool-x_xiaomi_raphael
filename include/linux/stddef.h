5344e726368acfc0929ecd52e610aae43fdff6e2
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-29 23:12:47 +0800

    Revert "UPSTREAM: stddef: Fix kerndoc for sizeof_field() and offsetofend()"
    This reverts commit f0d8b5c46dec74292e92d0698f43d1ec5c08c953.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/stddef.h
+++ include/linux/stddef.h
@@ -20,7 +20,7 @@ enum {
 #endif
 
 /**
- * sizeof_field() - Report the size of a struct field in bytes
+ * sizeof_field(TYPE, MEMBER)
  *
  * @TYPE: The structure containing the field of interest
  * @MEMBER: The field to return the size of
@@ -28,7 +28,7 @@ enum {
 #define sizeof_field(TYPE, MEMBER) sizeof((((TYPE *)0)->MEMBER))
 
 /**
- * offsetofend() - Report the offset of a struct field within the struct
+ * offsetofend(TYPE, MEMBER)
  *
  * @TYPE: The type of the structure
  * @MEMBER: The member within the structure to get the end offset of
