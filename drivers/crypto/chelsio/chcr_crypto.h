a6c6a75cf82289099959b16534ff33701d80734b
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-06-29 23:12:50 +0800

    Revert "BACKPORT: treewide: Replace open-coded flex arrays in unions"
    This reverts commit 2565d1d9db228d5df32c48de1be8cf553fdd78a8.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/crypto/chelsio/chcr_crypto.h
+++ drivers/crypto/chelsio/chcr_crypto.h
@@ -186,10 +186,8 @@ struct chcr_authenc_ctx {
 };
 
 struct __aead_ctx {
-	union {
-		DECLARE_FLEX_ARRAY(struct chcr_gcm_ctx, gcm);
-		DECLARE_FLEX_ARRAY(struct chcr_authenc_ctx, authenc);
-	};
+	struct chcr_gcm_ctx gcm[0];
+	struct chcr_authenc_ctx authenc[];
 };
 
 
@@ -215,11 +213,9 @@ struct hmac_ctx {
 };
 
 struct __crypto_ctx {
-	union {
-		DECLARE_FLEX_ARRAY(struct hmac_ctx, hmacctx);
-		DECLARE_FLEX_ARRAY(struct ablk_ctx, ablkctx);
-		DECLARE_FLEX_ARRAY(struct chcr_aead_ctx, aeadctx);
-	};
+	struct hmac_ctx hmacctx[0];
+	struct ablk_ctx ablkctx[0];
+	struct chcr_aead_ctx aeadctx[];
 };
 
 struct chcr_context {
