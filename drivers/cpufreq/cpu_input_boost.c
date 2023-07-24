5e43f67451168acba01e124c5ef6d5c31a80ecdc
John Galt <johngaltfirstrun@gmail.com>
2023-07-24 11:40:10 -0400

    cpu_input_boost: track last max boost
    Similar to last_input_time change, however this will be used for
    different fod and app launch behaviors to compete with libperfmgr.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -62,6 +62,7 @@ module_param(wake_boost_duration, short, 0644);
 #if(CONFIG_INPUT_BOOST_DURATION_MS != 0)
 unsigned long last_input_time;
 #endif
+unsigned long last_mb_time;
 
 enum {
 	SCREEN_OFF,
@@ -222,6 +223,7 @@ void cpu_input_boost_kick_max(unsigned int duration_ms, bool always)
 	struct boost_drv *b = &boost_drv_g;
 
 	__cpu_input_boost_kick_max(b, duration_ms, always);
+	last_mb_time = jiffies;
 }
 
 static void input_unboost_worker(struct work_struct *work)
