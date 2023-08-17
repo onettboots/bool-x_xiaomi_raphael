d3015097669bc42b4a355e7c1c7265e17f6a6dd0
pc1598 <piyushchouhan1598@gmail.com>
2023-08-17 19:28:46 +0000

    Revert "lib: zstd: Upgrade to latest upstream zstd version 1.5.2"
    This reverts commit a53dd19d65f3bc3dba2175e515ecc9e2f959a72f.

--- lib/zstd/decompress_sources.h
+++ lib/zstd/decompress_sources.h
@@ -16,12 +16,6 @@
  * decompression.
  */
 
-/*
- * Disable the ASM Huffman implementation because we need to
- * include all the sources.
- */
-#define ZSTD_DISABLE_ASM 1
-
 #include "common/debug.c"
 #include "common/entropy_common.c"
 #include "common/error_private.c"
