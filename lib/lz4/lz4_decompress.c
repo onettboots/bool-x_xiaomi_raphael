c89fabfde7716308a24e1de1b4b915fa3c19d14c
Onett Boots <90538882+onettboots@users.noreply.github.com>
2023-08-19 11:11:47 +0700

    Merge pull request #3 from Pc1598/dsp
     DSP branch if interested

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -915,8 +915,9 @@ int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
 				      noDict, (BYTE *)dest, NULL, 0);
 }
 
-int LZ4_decompress_safe_partial(const char *src, char *dst, int compressedSize,
-				int targetOutputSize, int dstCapacity)
+#if IS_ENABLED(CONFIG_EROFS_FS)
+int LZ4_decompress_safe_partial(const char *src, char *dst,
+	int compressedSize, int targetOutputSize, int dstCapacity)
 {
 	dstCapacity = min(targetOutputSize, dstCapacity);
 	return LZ4_decompress_generic(src, dst, compressedSize, dstCapacity,
