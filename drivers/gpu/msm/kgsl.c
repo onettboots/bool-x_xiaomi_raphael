6b8682a48b27c72818a244436eedd1fd4bd4e9b9
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-09-05 20:57:14 +0800

    Revert "affine stuff to prime"
    This reverts commit 8a142b3c6c2efc063ba0ba36db9aba6f0f7b7bd5.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5059,7 +5059,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	}
 
 	if (!strcmp(device->name, "kgsl-3d0")) {
-		irqflags |= IRQF_PRIME_AFFINE;
+		irqflags |= IRQF_PERF_AFFINE;
 	}
 
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
