f776438f1551a8164adabfcce7278973baf9935c
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-09-07 21:17:52 +0800

    Revert "[Adaptation] msm: kgsl: Affine kgsl_3d0_irq and worker kthread to the big CPU cluster"
    This reverts commit 13f579fe549f12aa008f3c4d9107f2c14acd8980.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4992,7 +4992,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
-	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -5058,12 +5057,9 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
-	if (!strcmp(device->name, "kgsl-3d0")) {
-		irqflags |= IRQF_PERF_AFFINE;
-	}
-
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				 kgsl_irq_handler, irqflags, device->name, device);
+				 kgsl_irq_handler, IRQF_TRIGGER_HIGH |
+				 IRQF_PERF_AFFINE, device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
 			      device->pwrctrl.interrupt_num, status);
