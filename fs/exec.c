c24be0440fb79a7bf26015e9345c477f753eed1d
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-09-29 20:51:42 +0800

    exec: adapt CLO tag merge for 8b480e7c3828b681f9aee82c8ac386d1f31faa55

--- fs/exec.c
+++ fs/exec.c
@@ -1834,9 +1834,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 		goto out_unmark;
 
 	bprm.argc = count(argv, MAX_ARG_STRINGS);
-	if (bprm->argc == 0)
+	if (bprm.argc == 0)
 		pr_warn_once("process '%s' launched '%s' with NULL argv: empty string added\n",
-			     current->comm, bprm->filename);
+			     current->comm, bprm.filename);
 	if ((retval = bprm.argc) < 0)
 		goto out;
 
@@ -1867,12 +1867,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	 * from argv[1] won't end up walking envp. See also
 	 * bprm_stack_limits().
 	 */
-	if (bprm->argc == 0) {
+	if (bprm.argc == 0) {
 		const char *argv[] = { "", NULL };
-		retval = copy_strings_kernel(1, argv, bprm);
+		retval = copy_strings_kernel(1, argv, &bprm);
 		if (retval < 0)
 			goto out;
-		bprm->argc = 1;
+		bprm.argc = 1;
 	}
 
 	retval = exec_binprm(&bprm);
