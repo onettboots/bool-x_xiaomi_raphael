6357588c4b53ac4c3683cd57cb748d329e319a59
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-10-11 15:50:51 +0200

    drivers: input: touchscreen: xiaomi: goodix_driver_gt9886: Implement double_tap node
    Change-Id: I128b90427ae7399a6d4b98b9f89b10a5c74deece
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.c
@@ -41,6 +41,10 @@
 #endif
 #include <linux/backlight.h>
 
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
+
 #define INPUT_EVENT_START						0
 #define INPUT_EVENT_SENSITIVE_MODE_OFF			0
 #define INPUT_EVENT_SENSITIVE_MODE_ON			1
@@ -848,6 +852,44 @@ static DEVICE_ATTR(irq_info, 0664,
 #ifdef CONFIG_TOUCHSCREEN_GOODIX_GTX8_GAMEMODE
 static DEVICE_ATTR(game_mode, 0664,
 		goodix_ts_game_mode_show, goodix_ts_game_mode_store);
+#endif
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+			       struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", goodix_core_data->double_wakeup);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+				struct kobj_attribute *attr, const char *buf,
+				size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	goodix_core_data->double_wakeup = !!val;
+	if (goodix_core_data->fod_status == -1 ||
+	    goodix_core_data->fod_status == 100)
+		goodix_core_data->gesture_enabled =
+			goodix_core_data->double_wakeup |
+			goodix_core_data->aod_status;
+	else
+		goodix_core_data->gesture_enabled =
+			goodix_core_data->double_wakeup |
+			goodix_core_data->fod_status |
+			goodix_core_data->aod_status;
+
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+	.show = double_tap_show,
+	.store = double_tap_store,
+};
 #endif
 
 static struct attribute *sysfs_attrs[] = {
@@ -2308,6 +2350,13 @@ static int goodix_ts_probe(struct platform_device *pdev)
 
 	/*create sysfs files*/
 	goodix_ts_sysfs_init(core_data);
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	r = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (r < 0) {
+		ts_err("Failed to create double_tap node err=%d\n", r);
+	}
+#endif
 
 	r = ts_device->hw_ops->reset(ts_device);
 	if (r < 0) {
