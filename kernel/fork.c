24e09707dcb14cf5d347351d9f39ff120a8162da
John Galt <johngaltfirstrun@gmail.com>
2023-11-15 14:33:17 -0500

    sbalance: balance on fork
    There are often new load and capacity requirements

--- kernel/fork.c
+++ kernel/fork.c
@@ -98,6 +98,7 @@
 
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/irq.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2264,6 +2265,7 @@ long _do_fork(unsigned long clone_flags,
 		cpu_input_boost_kick_max(500, false);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500, true);
 		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500, true);
+		balance_irqs();
 	}
 
 	/*
