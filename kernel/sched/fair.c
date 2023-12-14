e7f9be29417436c9c1a11224660170a543633b13
Wang Jinchao <wangjinchao@xfusion.com>
2023-12-14 13:20:29 +0800

    sched/fair: Remove unused 'next_buddy_marked' local variable in check_preempt_wakeup_fair()
    This variable became unused in:
    
        5e963f2bd465 ("sched/fair: Commit to EEVDF")
    
    Signed-off-by: Wang Jinchao <wangjinchao@xfusion.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/202312141319+0800-wangjinchao@xfusion.com
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8965,7 +8965,6 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	struct sched_entity *se = &curr->se, *pse = &p->se;
 	struct cfs_rq *cfs_rq = task_cfs_rq(curr);
 	int scale = cfs_rq->nr_running >= sched_nr_latency;
-	int next_buddy_marked = 0;
 
 	if (unlikely(se == pse))
 		return;
@@ -8981,7 +8980,6 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 
 	if (sched_feat(NEXT_BUDDY) && scale && !(wake_flags & WF_FORK)) {
 		set_next_buddy(pse);
-		next_buddy_marked = 1;
 	}
 
 	/*
