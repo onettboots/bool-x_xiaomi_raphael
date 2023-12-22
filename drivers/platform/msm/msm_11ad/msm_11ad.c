6fc24d75e253c75838797a9a40eeb2e391dd3787
onettboots <blackcocopet@gmail.com>
2023-12-22 10:24:09 +0000

    msm_11ad: fixups build
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/platform/msm/msm_11ad/msm_11ad.c
+++ drivers/platform/msm/msm_11ad/msm_11ad.c
@@ -29,7 +29,7 @@
 #include <linux/interrupt.h>
 #include <linux/cpumask.h>
 #include <linux/cpufreq.h>
-#include <linux/sched/core_ctl.h>
+//#include <linux/sched/core_ctl.h>
 #include "wil_platform.h"
 #include "msm_11ad.h"
 
@@ -1510,33 +1510,6 @@ static int ops_bus_request(void *handle, u32 kbps /* KBytes/Sec */)
 			"Failed msm_bus voting. kbps=%d vote=%d, rc=%d\n",
 			kbps, vote, rc);
 
-	if (ctx->use_cpu_boost) {
-		bool was_boosted = ctx->is_cpu_boosted;
-		bool needs_boost = (kbps >= WIGIG_MIN_CPU_BOOST_KBPS);
-
-		if (was_boosted != needs_boost) {
-			if (needs_boost) {
-				rc = core_ctl_set_boost(true);
-				if (rc) {
-					dev_err(ctx->dev,
-						"Failed enable boost rc=%d\n",
-						rc);
-					goto out;
-				}
-				msm_11ad_set_boost_affinity(ctx);
-				dev_dbg(ctx->dev, "CPU boost enabled\n");
-			} else {
-				rc = core_ctl_set_boost(false);
-				if (rc)
-					dev_err(ctx->dev,
-						"Failed disable boost rc=%d\n",
-						rc);
-				msm_11ad_clear_boost_affinity(ctx);
-				dev_dbg(ctx->dev, "CPU boost disabled\n");
-			}
-			ctx->is_cpu_boosted = needs_boost;
-		}
-	}
 out:
 	return rc;
 }
