817d18b27c1b50d669f3e5ab826e1a002e189aa2
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2024-01-24 22:06:47 +0800

    Revert "SBalance: Get the capacity of CPUs at its lowest frequency"
    This reverts commit 9afedc21cff92cfad5d6319a7202f83d220b6cc2.

--- kernel/irq/sbalance.c
+++ kernel/irq/sbalance.c
@@ -27,7 +27,6 @@
 
 #define pr_fmt(fmt) "sbalance: " fmt
 
-#include <linux/cpufreq.h>
 #include <linux/freezer.h>
 #include <linux/irq.h>
 #include <linux/list_sort.h>
@@ -221,11 +220,8 @@ extern void balance_irqs(void)
 	 * interrupts. That way, time spent processing each interrupt is
 	 * considered when balancing.
 	 */
-	for_each_cpu(cpu, &cpus) {
-		struct cpufreq_policy *policy = cpufreq_cpu_get(cpu);
-		per_cpu(cpu_cap, cpu) = arch_scale_cpu_capacity(NULL, cpu) *
-					policy->min / policy->max;
-	}
+	for_each_cpu(cpu, &cpus)
+		per_cpu(cpu_cap, cpu) = cpu_rq(cpu)->cpu_capacity;
 
 	list_for_each_entry_rcu(bi, &bal_irq_list, node) {
 		if (!update_irq_data(bi, &cpu))
