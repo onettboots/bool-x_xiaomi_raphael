654a42721bf7ceaac8c51b281379f4c6ea57b9c6
onettboots <blackcocopet@gmail.com>
2024-01-31 16:09:51 +0000

    Boolx-1.5
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export SUBARCH=arm64
 export ARCH=arm64
 export CC=$HOME/toolchains/boolx-clang/bin/clang
 
-VER="V1.4-Nethunter"
+VER="V1.5-Pickacak-LGC-Nethunter"
 KERNEL_DIR=`pwd`
 REPACK_DIR=$HOME/AnyKernel3
 ZIP_MOVE=$HOME/Boolx
