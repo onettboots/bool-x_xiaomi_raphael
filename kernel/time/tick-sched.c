847d9a34c718180b8828570a39aa80b4ed8062f1
onettboots <blackcocopet@gmail.com>
2024-03-19 18:22:14 +0000

    Merge tag 'v4.14.338-openela' of https://github.com/openela/kernel-lts into waf-dsp
    This is the 4.14.338 OpenELA-Extended LTS stable release

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -1421,13 +1421,18 @@ static inline void clear_tick_sched(struct tick_sched *ts)
 void tick_cancel_sched_timer(int cpu)
 {
 	struct tick_sched *ts = &per_cpu(tick_cpu_sched, cpu);
+	ktime_t idle_sleeptime, iowait_sleeptime;
 
 # ifdef CONFIG_HIGH_RES_TIMERS
 	if (ts->sched_timer.base)
 		hrtimer_cancel(&ts->sched_timer);
 # endif
 
-	clear_tick_sched(ts);
+	idle_sleeptime = ts->idle_sleeptime;
+	iowait_sleeptime = ts->iowait_sleeptime;
+	memset(ts, 0, sizeof(*ts));
+	ts->idle_sleeptime = idle_sleeptime;
+	ts->iowait_sleeptime = iowait_sleeptime;
 }
 #endif
 
