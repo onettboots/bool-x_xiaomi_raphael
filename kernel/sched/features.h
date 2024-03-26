e7a763b8b8650634df4906bf1216cce03ce8e4bf
onettboots <blackcocopet@gmail.com>
2024-03-26 16:11:32 +0000

    sched: drop slice and alt periode,
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -137,12 +137,3 @@
  * RT class.
  */
 #define SCHED_FEAT_SCHEDTUNE_BOOST_HOLD_ALL 0
-
-/*
- * Inflate the effective utilization of SchedTune-boosted tasks, which
- * generally leads to usage of higher frequencies.
- * If disabled, boosts will only bias tasks to higher-capacity CPUs.
- */
-
-#define SCHED_FEAT_ALT_PERIOD 1
-#define SCHED_FEAT_BASE_SLICE 1
