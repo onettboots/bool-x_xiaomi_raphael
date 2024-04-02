d87c97e781d5b82da29e64bbd8e795a934e0a593
onettboots <blackcocopet@gmail.com>
2024-04-02 16:31:19 +0000

    schedutil: enable iowait boost by default
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1018,7 +1018,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us = 1000;
 	tunables->hispeed_freq = 0;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
-	tunables->iowait_boost_enable = false;
+	tunables->iowait_boost_enable = true;
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 	stale_ns = sched_ravg_window + (sched_ravg_window >> 3);
