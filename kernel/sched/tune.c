e5674dc55561d113efe552cd1dab37c768fb50b4
Richard Raya <rdxzv.dev@gmail.com>
2024-05-08 00:12:54 -0300

    sched/tune: Restrict stune boost to top-app
    Change-Id: Ide30ce5cc10f441569f0b98de618e9e046c7db17
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -631,6 +631,11 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 {
 	struct schedtune *st = css_st(css);
 
+	if (!strcmp(css->cgroup->kn->name, "top-app"))
+		boost = 1;
+	else
+		boost = 0;
+
 	if (boost < 0 || boost > 100)
 		return -EINVAL;
 
