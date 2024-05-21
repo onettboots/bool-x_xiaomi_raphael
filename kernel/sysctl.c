66fa554dc0d029460e7627fe2d6d5dc4174fb21f
onettboots <blackcocopet@gmail.com>
2024-05-21 13:17:51 +0000

    kernel: sysctl: add missing stuff from missmerge fix grouping CONFIG_SCHED_DEBUG atually ts disabled add missing sysctl sched_asym_cap_sibling_freq_match_pct
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -334,6 +334,7 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
+#ifdef CONFIG_SCHED_DEBUG
 static int min_sched_granularity_ns = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns;			/* 0 usecs */
@@ -342,6 +343,7 @@ static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
+#endif /* CONFIG_SCHED_DEBUG */
 
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
@@ -455,6 +457,15 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
+	{
+                .procname       = "sched_asym_cap_sibling_freq_match_pct",
+                .data           = &sysctl_sched_asym_cap_sibling_freq_match_pct,
+                .maxlen         = sizeof(unsigned int),
+                .mode           = 0644,
+                .proc_handler   = proc_dointvec_minmax,
+                .extra1         = &one,
+                .extra2         = &one_hundred,
+        },
 #endif
 	{
 		.procname	= "sched_upmigrate",
@@ -479,6 +490,7 @@ static struct ctl_table kern_table[] = {
 		.extra1         = &zero,
 		.extra2         = &one,
 	},
+#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
@@ -537,7 +549,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
