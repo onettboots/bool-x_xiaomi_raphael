03a561301b596084f58a5ed0b584454d19f41e7e
onettboots <blackcocopet@gmail.com>
2024-05-27 10:38:50 +0000

    Revert "kernel: Fix cpufreq times memory leaks"
    This reverts commit 96c200fed57810f8806ceecf378c13b7b90f0188.

--- kernel/fork.c
+++ kernel/fork.c
@@ -2298,6 +2298,8 @@ long _do_fork(unsigned long clone_flags,
 		struct completion vfork;
 		struct pid *pid;
 
+		cpufreq_task_times_alloc(p);
+
 		trace_sched_process_fork(current, p);
 
 		pid = get_task_pid(p, PIDTYPE_PID);
