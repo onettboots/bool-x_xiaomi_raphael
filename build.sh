ee1e0018a37d38c9a3d7edd82b5a19c32aa7e167
onettboots <blackcocopet@gmail.com>
2024-06-12 13:59:14 +0000

    Boolx-1.8
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export SUBARCH=arm64
 export ARCH=arm64
 export CC=$HOME/toolchains/boolx-clang/bin/clang
 
-VER="V1.7-Pickasal-Miui-HyperOs"
+VER="V1.8-Picktobrut-Miui-HyperOs"
 KERNEL_DIR=`pwd`
 REPACK_DIR=$HOME/AnyKernel3
 ZIP_MOVE=$HOME/Boolx
