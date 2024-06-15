e76221dd0fe6925d2e3c05dba6bc05696ef86e8a
onettboots <blackcocopet@gmail.com>
2024-06-15 14:35:12 +0000

    Boolx-1.8
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ cpus=`expr $(nproc --all)`
 objdir="${kernel_dir}/out"
 CONFIGS="raphael_defconfig"
 
-VER="V1.7-Pickasal-Miui-HyperOs"
+VER="V1.8-PickTobrut-Miui-HyperOs"
 KERNEL_DIR=`pwd`
 REPACK_DIR=$HOME/AnyKernel3
 ZIP_MOVE=$HOME/Boolx
