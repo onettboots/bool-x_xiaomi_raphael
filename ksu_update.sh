f22e84b61bd6926b8e1d672710486d4111c9d4cf
onettboots <blackcocopet@gmail.com>
2024-06-17 20:25:08 +0700

    ksu: updater: squash to latest non GKI version 0.9.5 sadly latest ksu for non GKI has been dropped
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- ksu_update.sh
+++ ksu_update.sh
@@ -13,11 +13,11 @@ echo "Pulling main branch of KernelSU:"
 echo "--------------------------------"
 echo -e "${restore}"
 
-git subtree pull --prefix=drivers/kernelsu https://github.com/tiann/KernelSU.git main --squash -m "drivers: kernelsu: update"
+git subtree pull --prefix=drivers/kernelsu https://github.com/tiann/KernelSU.git v0.9.5 --squash -m "drivers: kernelsu: update"
 
 # Lets tricky calculating for the reall version here
 rm -rf ksu
-git clone https://github.com/tiann/KernelSU ksu
+git clone https://github.com/tiann/KernelSU -b v0.9.5 ksu
 cd ksu
 KSU_VERSION=10200
 KSU_GIT_VERSION=$(git rev-list --count HEAD)
@@ -41,7 +41,7 @@ sed -i "s/$OLD_VER/$NEW_VER/g" drivers/kernelsu/kernel/Makefile
 
 echo ""
 echo -e "${green}KernelSU has been Updated"
-echo -e "${restore}Curent version is ${blink_red}$CURRENT"
+echo -e "${restore}Curent version is ${blink_red}$CURRENT ${restore}"
 echo "$CURRENT" > drivers/kernelsu/kernel/dksu
 echo ""
 echo "Adding commit message:"
