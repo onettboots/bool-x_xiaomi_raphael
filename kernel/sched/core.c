b9adcace0fbc377cc1446616ce497d9dd0d8e3f8
onettboots <blackcocopet@gmail.com>
2024-07-30 20:41:37 +0700

    Revert "sched/core: decrease TA boost"
    This reverts commit 5d99b46c77ffe974c3bde53a8ca628093e6d3199.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -964,15 +964,10 @@ static inline void uclamp_boost_write(struct task_struct *p) {
 
 	//top-app min clamp input boost
 	if (strcmp(css->cgroup->kn->name, "top-app") == 0) {
-		if (kp_active_mode() == 3) {
+		if (kp_active_mode() == 3 || time_before(jiffies, last_input_time + msecs_to_jiffies(7000))) {
 			task_group(p)->uclamp[UCLAMP_MIN].value = 512;
-			task_group(p)->latency_sensitive = 1;
-		} else if (time_before(jiffies, last_input_time + msecs_to_jiffies(3500))) {
-			task_group(p)->uclamp[UCLAMP_MIN].value = 358;
-			task_group(p)->latency_sensitive = 1;
 		} else {
-			task_group(p)->uclamp[UCLAMP_MIN].value = 154;
-			task_group(p)->latency_sensitive = 0;
+			task_group(p)->uclamp[UCLAMP_MIN].value = 358;
 		}
 	}
 #ifdef CONFIG_STOCKISH_ROM_SUPPORT
