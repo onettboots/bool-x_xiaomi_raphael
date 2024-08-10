7e913e98be480926fa4f9f09e2b0bacc2ae68778
onettboots <blackcocopet@gmail.com>
2024-08-11 00:11:09 +0700

    Revert "memlat: Read perf counters in parallel and reduce system jitter"
    This reverts commit b1b0a2b6fc084ceb0d1964fc8cd08b187b5ef56d.

--- drivers/soc/qcom/scm.c
+++ drivers/soc/qcom/scm.c
@@ -433,12 +433,11 @@ static int ___scm_call_armv8_64(u64 x0, u64 x1, u64 x2, u64 x3, u64 x4, u64 x5,
 static int __scm_call_armv8_64(u64 x0, u64 x1, u64 x2, u64 x3, u64 x4, u64 x5,
 				u64 *ret1, u64 *ret2, u64 *ret3)
 {
-	atomic_t *cnt = per_cpu_ptr(&scm_call_count, raw_smp_processor_id());
 	int ret;
 
-	atomic_inc(cnt);
+	atomic_inc(&scm_call_count);
 	ret = ___scm_call_armv8_64(x0, x1, x2, x3, x4, x5, ret1, ret2, ret3);
-	atomic_dec(cnt);
+	atomic_dec(&scm_call_count);
 
 	return ret;
 }
@@ -496,12 +495,11 @@ static int ___scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 				u64 *ret1, u64 *ret2, u64 *ret3)
 {
-	atomic_t *cnt = per_cpu_ptr(&scm_call_count, raw_smp_processor_id());
 	int ret;
 
-	atomic_inc(cnt);
+	atomic_inc(&scm_call_count);
 	ret = ___scm_call_armv8_32(w0, w1, w2, w3, w4, w5, ret1, ret2, ret3);
-	atomic_dec(cnt);
+	atomic_dec(&scm_call_count);
 
 	return ret;
 }
@@ -559,12 +557,12 @@ static int ___scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 				u64 *ret1, u64 *ret2, u64 *ret3)
 {
-	atomic_t *cnt = per_cpu_ptr(&scm_call_count, raw_smp_processor_id());
+
 	int ret;
 
-	atomic_inc(cnt);
+	atomic_inc(&scm_call_count);
 	ret = ___scm_call_armv8_32(w0, w1, w2, w3, w4, w5, ret1, ret2, ret3);
-	atomic_dec(cnt);
+	atomic_dec(&scm_call_count);
 
 	return ret;
 }
