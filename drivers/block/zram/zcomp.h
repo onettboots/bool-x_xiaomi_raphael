2ae18837c4c454aa497aa0dc0503a615297b74c8
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-09-02 19:56:01 +0900

    zram: introduce zcomp_params structure
    We will store a per-algorithm parameters there (compression level,
    dictionary, dictionary size, etc.).
    
    Link: https://lkml.kernel.org/r/20240902105656.1383858-14-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nick Terrell <terrelln@fb.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: I7858fe5b95450fd19b765010519b8e2b6f5e2d29
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/block/zram/zcomp.h
+++ drivers/block/zram/zcomp.h
@@ -3,6 +3,14 @@
 #ifndef _ZCOMP_H_
 #define _ZCOMP_H_
 
+#define ZCOMP_PARAM_NO_LEVEL	INT_MIN
+
+struct zcomp_params {
+	void *dict;
+	size_t dict_sz;
+	s32 level;
+};
+
 struct zcomp_strm {
 	/* compression/decompression buffer */
 	void *buffer;
@@ -16,7 +24,7 @@ struct zcomp_ops {
 	int (*decompress)(void *ctx, const unsigned char *src, size_t src_len,
 			  unsigned char *dst, size_t dst_len);
 
-	void *(*create_ctx)(void);
+	void *(*create_ctx)(struct zcomp_params *params);
 	void (*destroy_ctx)(void *ctx);
 
 	const char *name;
@@ -26,6 +34,7 @@ struct zcomp_ops {
 struct zcomp {
 	struct zcomp_strm __percpu *stream;
 	const struct zcomp_ops *ops;
+	struct zcomp_params *params;
 	struct hlist_node node;
 };
 
@@ -34,7 +43,7 @@ int zcomp_cpu_dead(unsigned int cpu, struct hlist_node *node);
 ssize_t zcomp_available_show(const char *comp, char *buf);
 bool zcomp_available_algorithm(const char *comp);
 
-struct zcomp *zcomp_create(const char *alg);
+struct zcomp *zcomp_create(const char *alg, struct zcomp_params *params);
 void zcomp_destroy(struct zcomp *comp);
 
 struct zcomp_strm *zcomp_stream_get(struct zcomp *comp);
