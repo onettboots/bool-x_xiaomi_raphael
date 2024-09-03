51eb039a1e264331ba84f5f1ccae7689c8bd68d8
onettboots <blackcocopet@gmail.com>
2024-09-03 16:59:00 +0000

    Revert "schedutil: enable iowait boost by default"
    This reverts commit 454e79f4072b52cef00ca11c4b15d2f042862d04.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -791,7 +791,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 	tunables->up_rate_limit_us = 500;
 	tunables->down_rate_limit_us = 1000;
-	tunables->iowait_boost_enable = true;
+	tunables->iowait_boost_enable = false;
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 	stale_ns = sched_ravg_window + (sched_ravg_window >> 3);
