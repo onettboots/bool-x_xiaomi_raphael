52f30e0f44888a0f0fee773ffc2d3850246e9102
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-09-17 11:09:09 +0900

    zram: rework writeback target selection strategy
    Writeback suffers from the same problem as recompression did before -
    target slot selection for writeback is just a simple iteration over
    zram->table entries (stored pages) which selects suboptimal targets for
    writeback.  This is especially problematic for writeback, because we
    uncompress objects before writeback so each of them takes 4K out of
    limited writeback storage.  For example, when we take a 48 bytes slot and
    store it as a 4K object to writeback device we only save 48 bytes of
    memory (release from zsmalloc pool).  We naturally want to pick the
    largest objects for writeback, because then each writeback will release
    the largest amount of memory.
    
    This patch applies the same solution and strategy as for recompression
    target selection: pp control (post-process) with 16 buckets of candidate
    pp slots.  Slots are assigned to pp buckets based on sizes - the larger
    the slot the higher the group index.  This gives us sorted by size lists
    of candidate slots (in linear time), so that among post-processing
    candidate slots we always select the largest ones first and maximize the
    memory saving.
    
    TEST
    ====
    
    A very simple demonstration: zram is configured with a writeback device.
    A limited writeback (wb_limit 2500 pages) is performed then, with a log of
    sizes of slots that were written back.  You can see that patched zram
    selects slots for recompression in significantly different manner, which
    leads to higher memory savings (see column #2 of mm_stat output).
    
    BASE
    ----
    
    *** initial state of zram device
    /sys/block/zram0/mm_stat
    1750327296 619765836 631902208        0 631902208        1        0    34278    34278
    
    *** writeback idle wb_limit 2500
    /sys/block/zram0/mm_stat
    1750327296 617622333 631578624        0 631902208        1        0    34278    34278
    
    Sizes of selected objects for writeback:
    ... 193 349 46 46 46 46 852 1002 543 162 107 49 34 34 34 ...
    
    PATCHED
    -------
    
    *** initial state of zram device
    /sys/block/zram0/mm_stat
    1750319104 619760957 631992320        0 631992320        1        0    34278    34278
    
    *** writeback idle wb_limit 2500
    /sys/block/zram0/mm_stat
    1750319104 612672056 626135040        0 631992320        1        0    34278    34278
    
    Sizes of selected objects for writeback:
    ... 3667 3580 3581 3580 3581 3581 3581 3231 3211 3203 3231 3246 ...
    
    Note, pp-slots are not strictly sorted, there is a PP_BUCKET_SIZE_RANGE
    variation of sizes within particular bucket.
    
    Link: https://lkml.kernel.org/r/20240917021020.883356-5-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Ic4e9df9ad179c787f234d1e795b4b76ebe482730
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -180,7 +180,7 @@ static void update_position(u32 *index, int *offset, struct bio_vec *bvec)
 	*offset = (*offset + bvec->bv_len) % PAGE_SIZE;
 }
 
-#ifdef CONFIG_ZRAM_MULTI_COMP
+#if defined CONFIG_ZRAM_WRITEBACK || defined CONFIG_ZRAM_MULTI_COMP
 struct zram_pp_slot {
 	unsigned long		index;
 	struct list_head	entry;
@@ -720,12 +720,58 @@ static void read_from_bdev_async(struct zram *zram, struct page *page,
 #define IDLE_WRITEBACK			(1<<1)
 #define INCOMPRESSIBLE_WRITEBACK	(1<<2)
 
+static int scan_slots_for_writeback(struct zram *zram, u32 mode,
+				    unsigned long nr_pages,
+				    unsigned long index,
+				    struct zram_pp_ctl *ctl)
+{
+	struct zram_pp_slot *pps = NULL;
+
+	for (; nr_pages != 0; index++, nr_pages--) {
+		if (!pps)
+			pps = kmalloc(sizeof(*pps), GFP_KERNEL);
+		if (!pps)
+			return -ENOMEM;
+
+		INIT_LIST_HEAD(&pps->entry);
+
+		zram_slot_lock(zram, index);
+		if (!zram_allocated(zram, index))
+			goto next;
+
+		if (zram_test_flag(zram, index, ZRAM_WB) ||
+		    zram_test_flag(zram, index, ZRAM_SAME))
+			goto next;
+
+		if (mode & IDLE_WRITEBACK &&
+		    !zram_test_flag(zram, index, ZRAM_IDLE))
+			goto next;
+		if (mode & HUGE_WRITEBACK &&
+		    !zram_test_flag(zram, index, ZRAM_HUGE))
+			goto next;
+		if (mode & INCOMPRESSIBLE_WRITEBACK &&
+		    !zram_test_flag(zram, index, ZRAM_INCOMPRESSIBLE))
+			goto next;
+
+		pps->index = index;
+		place_pp_slot(zram, ctl, pps);
+		pps = NULL;
+next:
+		zram_slot_unlock(zram, index);
+	}
+
+	kfree(pps);
+	return 0;
+}
+
 static ssize_t writeback_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
 	struct zram *zram = dev_to_zram(dev);
 	unsigned long nr_pages = zram->disksize >> PAGE_SHIFT;
-	unsigned long index;
+	struct zram_pp_ctl *ctl = NULL;
+	struct zram_pp_slot *pps;
+	unsigned long index = 0;
 	struct bio bio;
 	struct bio_vec bio_vec;
 	struct page *page;
@@ -785,7 +831,15 @@ static ssize_t writeback_store(struct device *dev,
 		goto release_init_lock;
 	}
 
-	for (; nr_pages != 0; index++, nr_pages--) {
+	ctl = init_pp_ctl();
+	if (!ctl) {
+		ret = -ENOMEM;
+		goto release_init_lock;
+	}
+
+	scan_slots_for_writeback(zram, mode, nr_pages, index, ctl);
+
+	while ((pps = select_pp_slot(ctl))) {
 		spin_lock(&zram->wb_limit_lock);
 		if (zram->wb_limit_enable && !zram->bd_wb_limit) {
 			spin_unlock(&zram->wb_limit_lock);
@@ -802,25 +856,10 @@ static ssize_t writeback_store(struct device *dev,
 			}
 		}
 
+		index = pps->index;
 		zram_slot_lock(zram, index);
-		if (!zram_allocated(zram, index))
+		if (!zram_test_flag(zram, index, ZRAM_PP_SLOT))
 			goto next;
-
-		if (zram_test_flag(zram, index, ZRAM_WB) ||
-				zram_test_flag(zram, index, ZRAM_SAME) ||
-				zram_test_flag(zram, index, ZRAM_UNDER_WB))
-			goto next;
-
-		if (mode & IDLE_WRITEBACK &&
-		    !zram_test_flag(zram, index, ZRAM_IDLE))
-			goto next;
-		if (mode & HUGE_WRITEBACK &&
-		    !zram_test_flag(zram, index, ZRAM_HUGE))
-			goto next;
-		if (mode & INCOMPRESSIBLE_WRITEBACK &&
-		    !zram_test_flag(zram, index, ZRAM_INCOMPRESSIBLE))
-			goto next;
-
 		/*
 		 * Clearing ZRAM_UNDER_WB is duty of caller.
 		 * IOW, zram_free_page never clear it.
@@ -834,6 +873,8 @@ static ssize_t writeback_store(struct device *dev,
 			zram_clear_flag(zram, index, ZRAM_UNDER_WB);
 			zram_clear_flag(zram, index, ZRAM_IDLE);
 			zram_slot_unlock(zram, index);
+
+			release_pp_slot(zram, pps);
 			continue;
 		}
 
@@ -852,6 +893,8 @@ static ssize_t writeback_store(struct device *dev,
 			zram_clear_flag(zram, index, ZRAM_UNDER_WB);
 			zram_clear_flag(zram, index, ZRAM_IDLE);
 			zram_slot_unlock(zram, index);
+
+			release_pp_slot(zram, pps);
 			/*
 			 * BIO errors are not fatal, we continue and simply
 			 * attempt to writeback the remaining objects (pages).
@@ -894,6 +937,7 @@ static ssize_t writeback_store(struct device *dev,
 		spin_unlock(&zram->wb_limit_lock);
 next:
 		zram_slot_unlock(zram, index);
+		release_pp_slot(zram, pps);
 	}
 
 	if (blk_idx)
@@ -901,6 +945,7 @@ next:
 	ret = len;
 	__free_page(page);
 release_init_lock:
+	release_pp_ctl(zram, ctl);
 	atomic_set(&zram->pp_in_progress, 0);
 	up_read(&zram->init_lock);
 
