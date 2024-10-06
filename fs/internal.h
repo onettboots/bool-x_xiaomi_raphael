c836e72c1f2f1e00be8c9c17b09cdaef7b18533c
onettboots <blackcocopet@gmail.com>
2024-10-06 23:14:47 +0700

    fs: Add SUSFS patch source : https://gitlab.com/simonpunk/susfs4ksu/-/blob/master/kernel_patches/50_add_susfs_in_kernel-4.14.patch?ref_type=heads
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/internal.h
+++ fs/internal.h
@@ -76,6 +76,10 @@ extern void __mnt_drop_write(struct vfsmount *);
 extern void __mnt_drop_write_file(struct file *);
 extern void mnt_drop_write_file_path(struct file *);
 
+#ifdef CONFIG_KSU_SUSFS
+int path_umount(struct path *path, int flags);
+#endif
+
 /*
  * fs_struct.c
  */
