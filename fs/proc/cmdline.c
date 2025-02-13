b6173afc9bfd8ac33d25418c48cad89b8dee0345
onettboots <blackcocopet@gmail.com>
2025-02-13 15:03:22 +0000

    fs: Add SUSFS v1.5.5 patch
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,12 +4,21 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <asm/setup.h>
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
 
 static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", new_command_line);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
+	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
 
