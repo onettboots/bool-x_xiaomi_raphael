910243d0c282792d2364122232593d26b55706b9
onettboots <blackcocopet@gmail.com>
2025-03-20 22:18:29 +0700

    mm: change swappiness to 100
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -170,7 +170,7 @@ unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 100;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
