ddb3eb2801ad37ce04b5f8be19d2f6f3c72c01a2
zenkernol-dev <taufikdaulay320@gmail.com>
2025-05-25 18:06:52 +0700

    sched: core: tweaks perfomance for games CODM
    - UnityGfxDeviceW process added, which is as heavy as UnityMain
    
    Signed-off-by: zenkernol-dev <taufikdaulay320@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4928,7 +4928,7 @@ static bool task_is_unity_game(struct task_struct *p)
 		rcu_read_lock();
 		for_each_thread(p, t) {
 			/* Check for a UnityMain thread in the thread group */
-			if (!strcmp(t->comm, "UnityMain")) {
+			if (!strcmp(t->comm, "UnityMain") || !strcmp(t->comm, "UnityGfxDeviceW")) {
 				ret = true;
 				break;
 			}
