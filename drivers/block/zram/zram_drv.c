86799a5a48728507d0ccd815bafa53a70ed3bcc1
EmanuelCN <emanuelghub@gmail.com>
2025-06-01 22:04:45 +0300

    Revert "zram: Do not allow compression algorithm to be changed"
    This reverts commit 94cd052bcf5347765a0bc1bfb6c8c6f5593ea7be.
    
    Change-Id: I1970c935624b03b162f06bbc43fe7754fe63664e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1181,7 +1181,6 @@ static ssize_t __comp_algorithm_show(struct zram *zram, u32 prio, char *buf)
 
 static int __comp_algorithm_store(struct zram *zram, u32 prio, const char *buf)
 {
-#if 0
 	char *compressor;
 	size_t sz;
 
@@ -1212,7 +1211,6 @@ static int __comp_algorithm_store(struct zram *zram, u32 prio, const char *buf)
 
 	comp_algorithm_set(zram, prio, compressor);
 	up_write(&zram->init_lock);
-#endif
 	return 0;
 }
 
