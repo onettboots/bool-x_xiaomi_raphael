f382136cb54b942ada528597635017b36f26f176
onettboots <blackcocopet@gmail.com>
2025-06-12 19:02:50 +0700

    sys: fixup after drop susfs
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1212,13 +1212,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "netbpfload", 10) &&
-	    current->pid != netbpfload_pid) {
-		netbpfload_pid = current->pid;
-		strcpy(tmp.release, "6.6.40");
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
-	}
+
 	up_read(&uts_sem);
 
 	rcu_read_lock();
