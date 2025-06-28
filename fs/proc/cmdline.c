8668bf051440ac746fa2fc8df1443984877f5404
onettboots <blackcocopet@gmail.com>
2025-06-28 07:28:32 +0000

    Revert "init: Add CONFIG_INITRAMFS_IGNORE_SKIP_FLAG"
    This reverts commit 6cb1c7dd635e76a046ccb9e4c01058a375f09449.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,16 +3,9 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#include <asm/setup.h>
-#endif
-
-static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-	seq_printf(m, "%s\n", proc_command_line);
-#else
 	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
@@ -53,14 +46,6 @@ static void remove_safetynet_flags(char *cmd)
 
 static int __init proc_cmdline_init(void)
 {
-	strcpy(new_command_line, saved_command_line);
-
-	/*
-	 * Remove various flags from command line seen by userspace in order to
-	 * pass SafetyNet CTS check.
-	 */
-	remove_safetynet_flags(new_command_line);
-
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
