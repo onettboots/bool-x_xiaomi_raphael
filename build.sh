598642556325e9cdd249a9beaa0e4218550bcd55
onettboots <blackcocopet@gmail.com>
2025-06-30 17:23:03 +0000

    script: Update
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -44,6 +44,13 @@ CONFIG=out/.config
 SAVEHERE=$HOME/toolchains
 KERNEL=out/arch/arm64/boot/Image.gz-dtb
 DTBO=out/arch/arm64/boot/dtbo.img
+upl=$kernel_dir/upl.sh
+export THINLTO_CACHE_PATH=$SAVEHERE/thincache
+KER_VER=$(grep -oP '(?<=VERSION = )\d+|(?<=PATCHLEVEL = )\d+|(?<=SUBLEVEL = )\d+' Makefile | paste -sd '.')
+KSU_VER=$(cat drivers/kernelsu/kernel/dksu 2>/dev/null || echo "Disabled")
+SUSFS_VER=$(grep -oP '(?<=#define SUSFS_VERSION ")[^"]*' include/linux/susfs.h 2>/dev/null || echo "Disabled")
+OCDS=$(grep -qP "timing@1\s*{" arch/arm64/boot/dts/qcom/dsi-panel-ss-fhd-ea8076-cmd.dtsi && echo "OCD" || echo "No-OCD")
+export THINLTO_CACHE_DIR=$HOME/toolchains/thincache
 
 #functions
 function makeconfig() {
@@ -88,7 +95,7 @@ function make_config {
 		makeconfig ${CONFIGS}
 }
 function make_boot {
-		cp $KERNEL $REPACK_DIR && cp $DTBO $REPACK_DIR/oc
+		cp $KERNEL $REPACK_DIR && cp $DTBO $REPACK_DIR
 }
 function make_zip {
 		cd $REPACK_DIR
@@ -105,18 +112,16 @@ function upload()
 function upload_boolx_action()
 {
         ziped=$ZIP_MOVE/`echo $ZIP_NAME`.zip
-        upl=$kernel_dir/upl.sh
-        rm -rf $upl
         cd $kernel_dir
         #wget
         chmod +x $upl
         sed -i "4i\FILE_PATH=$ziped" $upl
         BUILDDATE=`date +"%Y-%m-%d"`
         sed -i '5i\CAPTION="* Build Date: '$BUILDDATE'' $upl
-        sed -i '6i\* Kernel Version: v.4.14.356' $upl
-        sed -i '7i\* KSU+NEXT: v.12465' $upl
-        sed -i '8i\* SUSFS: v1.5.5' $upl
-        sed -i '9i\* Type: Legacy, Mi-Thermal, OCD' $upl
+        sed -i '6i\* Kernel Version: '$KER_VER'' $upl
+        sed -i '7i\* KSU+NEXT: '$KSU_VER'' $upl
+        sed -i '8i\* SUSFS: '$SUSFS_VER'' $upl
+        sed -i '9i\* Type: Legacy, '$OCDS'' $upl
         sed -i '10i\* Changes: https://github.com/onettboots/bool-x_xiaomi_raphael/commits/14-LGCocd' $upl
         sed -i '11i\* Clang: Boolx Clang 21.0.0"' $upl
         bash $upl
@@ -136,17 +141,19 @@ if [ -d $TOOLCHAINS ]; then
    echo -e "${restore}"
 else
    echo -e "${red}"
-   echo "Downloading toolchains.!!"
-   while read -p "what your architecture (aarch64/x86)? " cchoice
+   echo "Toolchains Architecture Host:"
+   echo "1. ARCH64"
+   echo "2. X86"
+   while read -p "Choose your architecture (1 / 2)? " cchoice
 do
 case "$cchoice" in
-	aarch64|Aarch64 )
+	1 )
 		echo
 		echo "Downloading Boolx-clang for Aarch64 host."
 		git clone https://gitlab.com/onettboots/boolx-clang.git -b Clang-15.0 $TOOLCHAINS
 		break
 		;;
-	x86|X86 )
+	2 )
 		echo
 		echo "Downloading Boolx-clang for X86 host."
 		wget https://github.com/onettboots/boolx-clang-build/releases/download/Boolx-21/boolx-clang21.tar.gz -P $SAVEHERE
@@ -296,7 +303,11 @@ if [ -f $KERNEL ]; then
    echo "------------------------------------------"
    echo -e "${restore}"
    build_time
-   upload
+   if [ -f $upl ]; then
+        upload_boolx_action
+   else
+        upload
+   fi
    echo
 else
    echo -e "${red}"
@@ -308,3 +319,4 @@ else
 fi
 
 echo
+rm -rf $upl
