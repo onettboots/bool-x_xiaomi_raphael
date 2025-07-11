0c0e11466b483f592dceb79e48f0b1e645904ee6
onettboots <blackcocopet@gmail.com>
2025-07-11 20:22:52 +0700

    Revert "sched/fair: Remove throughput optimization that keeps tasks on big CPUs"
    This reverts commit 7c57f9647f063e4de7594b30245777d3dc3de032.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10711,6 +10711,16 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 		goto asym_packing;
 
 	/*
+	 * Candidate sg has no more than one task per CPU and
+	 * has higher per-CPU capacity. Migrating tasks to less
+	 * capable CPUs may harm throughput. Maximize throughput,
+	 * power/energy consequences are not considered.
+	 */
+	if (sgs->sum_nr_running <= sgs->group_weight &&
+	    capacity_greater(sg->sgc->min_capacity, capacity_of(env->dst_cpu)))
+		return false;
+
+	/*
 	 * If we have more than one misfit sg go with the biggest misfit.
 	 */
 	if (sgs->group_type == group_misfit_task &&
@@ -11486,7 +11496,6 @@ asym_active_balance(struct lb_env *env)
 static inline bool
 voluntary_active_balance(struct lb_env *env)
 {
-	struct sched_domain *sd = env->sd;
 	if (asym_active_balance(env))
 		return 1;
 
