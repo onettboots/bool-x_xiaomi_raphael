3b61f75005341d17c7232cfb6fc233ae27cce765
onettboots <blackcocopet@gmail.com>
2025-07-11 20:22:30 +0700

    Revert "cpufreq: arch_topology: Introduce minimum frequency scale"
    This reverts commit 2eae867b3fc81c43439f0946609f2192beb86687.

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -54,11 +54,4 @@ unsigned long topology_get_max_freq_scale(struct sched_domain *sd, int cpu)
 		   per_cpu(max_thermal_scale, cpu));
 }
 
-DECLARE_PER_CPU(unsigned long, arch_min_freq_scale);
-
-static inline unsigned long topology_get_min_freq_scale(int cpu)
-{
-	return per_cpu(arch_min_freq_scale, cpu);
-}
-
 #endif /* _LINUX_ARCH_TOPOLOGY_H_ */
