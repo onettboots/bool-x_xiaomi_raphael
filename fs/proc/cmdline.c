e1b811246519a020bd6d6d0266c245f0b500451d
onettboots <blackcocopet@gmail.com>
2025-07-15 22:05:17 +0700

    fs: cmdline: fixups
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -32,28 +32,6 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-static void remove_flag(char *cmd, const char *flag)
-{
-	char *start_addr, *end_addr;
-
-	/* Ensure all instances of a flag are removed */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memmove(start_addr, end_addr + 1, strlen(end_addr));
-		else
-			*(start_addr - 1) = '\0';
-	}
-}
-
-static void remove_safetynet_flags(char *cmd)
-{
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
-	remove_flag(cmd, "androidboot.veritymode=");
-}
-
 static int __init proc_cmdline_init(void)
 {
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
