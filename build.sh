c9a0d706c4bf19fe0401728f2c74371140b97229
onettboots <blackcocopet@gmail.com>
2025-07-16 21:18:07 +0700

    script: update
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ restore='\033[0m'
 clear
 
 # Resources
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 10G
 export SUBARCH=arm64
 export ARCH=arm64
 export CLANG_PATH="$HOME/toolchains/boolx-clang/bin"
@@ -29,7 +31,7 @@ cpus=`expr $(nproc --all)`
 objdir="${kernel_dir}/out"
 CONFIGS="raphael_defconfig"
 
-VER="V1.9-PickSakahayang-DSP"
+VER="V2.3-Parvez-DSP-PELT"
 KERNEL_DIR=`pwd`
 REPACK_DIR=$HOME/AnyKernel3
 ZIP_MOVE=$HOME/Boolx
@@ -38,9 +40,17 @@ DATE=`date +"%Y%m%d-%H%M"`
 AK_VER="$BASE_AK_VER$VER"
 ZIP_NAME="$AK_VER"-"$DATE"
 TOOLCHAINS=$HOME/toolchains/boolx-clang
+SAVEHERE=$HOME/toolchains
 CONFIG=out/.config
 KERNEL=out/arch/arm64/boot/Image.gz-dtb
 DTBO=out/arch/arm64/boot/dtbo.img
+upl=$kernel_dir/upl.sh
+export THINLTO_CACHE_PATH=$SAVEHERE/thincache
+KER_VER=$(grep -oP '(?<=VERSION = )\d+|(?<=PATCHLEVEL = )\d+|(?<=SUBLEVEL = )\d+' Makefile | paste -sd '.')
+KSU_VER=$(cat drivers/kernelsu/kernel/dksu 2>/dev/null || echo "Disabled")
+SUSFS_VER=$(grep -oP '(?<=#define SUSFS_VERSION ")[^"]*' include/linux/susfs.h 2>/dev/null || echo "Disabled")
+OCDS=$(grep -qP "timing@1\s*{" arch/arm64/boot/dts/qcom/dsi-panel-ss-fhd-ea8076-cmd.dtsi && echo "OCD" || echo "Non OCD")
+export THINLTO_CACHE_DIR=$HOME/toolchains/thincache
 
 #functions
 function makeconfig() {
@@ -85,13 +95,36 @@ function make_config {
 		makeconfig ${CONFIGS}
 }
 function make_boot {
-		cp $KERNEL $REPACK_DIR && cp $DTBO $REPACK_DIR/oc
+		cp $KERNEL $REPACK_DIR && cp $DTBO $REPACK_DIR
 }
 function make_zip {
 		cd $REPACK_DIR
 		zip -r9 `echo $ZIP_NAME`.zip *
 		mv  `echo $ZIP_NAME`*.zip $ZIP_MOVE
 		cd $KERNEL_DIR
+}
+
+function upload()
+{
+curl bashupload.com -T $ZIP_NAME*.zip
+}
+
+function upload_boolx_action()
+{
+                ziped=$ZIP_MOVE/`echo $ZIP_NAME`.zip
+		cd $kernel_dir
+		#wget
+		chmod +x $upl
+		sed -i "4i\FILE_PATH=$ziped" $upl
+		BUILDDATE=`date +"%Y-%m-%d"`
+		sed -i '5i\CAPTION="* Build Date: '$BUILDDATE'' $upl
+		sed -i '6i\* Kernel Version: '$KER_VER'' $upl
+		sed -i '7i\* KSU+NEXT: '$KSU_VER'' $upl
+		sed -i '8i\* SUSFS: '$SUSFS_VER'' $upl
+		sed -i '9i\* Type: DSP, PELT+CASS, '$OCDS'' $upl
+		sed -i '10i\* Changes: https://github.com/onettboots/bool-x_xiaomi_raphael/commits/PeltCass' $upl
+                sed -i '11i\* Clang: Boolx Clang 21.0.0"' $upl
+                bash $upl
 }
 
 DATE_START=$(date +"%s")
@@ -108,27 +141,32 @@ if [ -d $TOOLCHAINS ]; then
    echo -e "${restore}"
 else
    echo -e "${red}"
-   echo "Downloading toolchains.!!"
-   while read -p "what your architecture (aarch64/x86)? " cchoice
+   echo "Toolchains Architecture Host:"
+   echo "1. ARCH64"
+   echo "2. X86"
+   while read -p "Choose your architecture (1 / 2)? " cchoice
 do
 case "$cchoice" in
-	aarch64|Aarch64 )
-		echo
-		echo "Downloading Boolx-clang for Aarch64 host."
-		git clone https://gitlab.com/onettboots/boolx-clang.git -b Clang-15.0 $TOOLCHAINS
-		break
-		;;
-	x86|X86 )
-		echo
-		echo "Downloading Boolx-clang for X86 host."
-		git clone https://gitlab.com/onettboots/boolx-clang.git -b Clang-17.0_x86 $TOOLCHAINS
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
+        1 )
+                echo
+                echo "Downloading Boolx-clang for Aarch64 host."
+                git clone https://gitlab.com/onettboots/boolx-clang.git -b Clang-15.0 $TOOLCHAINS
+                break
+                ;;
+        2 )
+                echo
+                echo "Downloading Boolx-clang for X86 host."
+                wget https://github.com/onettboots/boolx-clang-build/releases/download/Boolx-21/boolx-clang21.tar.gz -P $SAVEHERE
+                cd $SAVEHERE
+                echo "Extracting Boolx Clang 21.0.0 to $HOME/toolchains/:"
+                tar -xf boolx-clang21.tar.gz
+                break
+                ;;
+        * )
+                echo
+                echo "Invalid try again!"
+                echo
+                ;;
 esac
 done
    echo -e "${restore}"
@@ -156,7 +194,7 @@ echo -e "${green}"
 echo "------------------"
 echo "CLEAN OPTIONS:"
 echo "------------------"
-while read -p "Do you want to clean stuffs (y/n)? " cchoice
+while read -p "Do you want to clean build (y/n)? " cchoice
 do
 case "$cchoice" in
 	y|Y )
@@ -230,6 +268,12 @@ echo -e "${restore}"
 cd ${kernel_dir}
 build ${TARGET_IMAGE}
 
+function build_time {
+   DATE_END=$(date +"%s")
+   DIFF=$(($DATE_END - $DATE_START))
+   echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+}
+
 echo -e "${green}"
 echo "----------------------"
 echo "Checking output files"
@@ -252,15 +296,22 @@ if [ -f $KERNEL ]; then
    echo $ZIP_NAME*.zip
    echo "------------------------------------------"
    echo -e "${restore}"
+   build_time
+   if [ -f $upl ]; then
+   	upload_boolx_action
+   else
+   	upload
+   fi
+   echo
 else
    echo -e "${red}"
    echo "-------------------------------------"
    echo "Building failed, Fix it and rebuild...!!!"
    echo "-------------------------------------"
    echo -e "${restore}"
+   build_time
 fi
 
-DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
-echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 echo
+
+rm -rf $upl
