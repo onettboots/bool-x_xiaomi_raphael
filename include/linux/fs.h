31e7491cfc8bc9710354fdc4b945b1d57db95362
onettboots <blackcocopet@gmail.com>
2025-10-04 20:51:19 +0700

    fs: fixup
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -608,7 +608,7 @@ struct inode {
 	 *    inode_(inc|dec)_link_count
 	 */
 	union {
-		const unsigned int i_nlink = {0};
+		const unsigned int i_nlink;
 		unsigned int __i_nlink;
 	};
 	dev_t			i_rdev;
