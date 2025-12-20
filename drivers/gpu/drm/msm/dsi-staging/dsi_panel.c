796274b866e65a9b1cbccb1c6e852c01f4643286
firebird11 <hbgassel@gmail.com>
2025-12-20 01:42:29 +0100

    Display: Add HBM
    Change-Id: Ib5b4023af234418754402d4d5dacfe9ba68faa3b
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -786,6 +786,40 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 			panel->fod_dim_lut[i - 1].alpha, panel->fod_dim_lut[i].alpha);
 }
 
+int dsi_panel_set_hbm(struct dsi_panel *panel, bool status)
+{
+    int rc = 0;
+
+    if (!panel)
+        return -EINVAL;
+
+    panel->hbm_requested = status;
+
+    if (!dsi_panel_initialized(panel)) {
+        pr_info("HBM deferred (panel not initialized), req=%d\n", status);
+        return 0;
+    }
+
+	if (status) {
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_HBM_ON);
+		if (rc)
+			pr_err("[%s] failed to send DSI_CMD_SET_DISP_HBM_ON cmd, rc=%d\n",
+					panel->name, rc);
+	} else {
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_HBM_OFF);
+		if (rc)
+			pr_err("[%s] failed to send DSI_CMD_SET_DISP_HBM_OFF cmd, rc=%d\n",
+					panel->name, rc);
+	}
+
+    if (!panel || !dsi_panel_initialized(panel)) {
+        pr_warn("HBM ignored: panel not initialized\n");
+        return -EAGAIN;
+    }
+
+	return rc;
+}
+
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
 	int rc = 0;
@@ -1201,10 +1235,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	default:
 		pr_err("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
-	}
-
-	if (last_backlight == 0 && panel->cphy_esd_check) {
-		schedule_delayed_work(&panel->esd_work, msecs_to_jiffies(300));
 	}
 
 	return rc;
@@ -2297,6 +2327,8 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-post-mode-switch-on-command",
 	"qcom,mdss-dsi-qsync-on-commands",
 	"qcom,mdss-dsi-qsync-off-commands",
+    "qcom,mdss-dsi-dispparam-hbm-on-command",
+    "qcom,mdss-dsi-dispparam-hbm-off-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command",
 	"qcom,mdss-dsi-esd-check-read-command",
@@ -2332,6 +2364,8 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-post-mode-switch-on-command-state",
 	"qcom,mdss-dsi-qsync-on-commands-state",
 	"qcom,mdss-dsi-qsync-off-commands-state",
+    "qcom,mdss-dsi-dispparam-hbm-on-command-state",
+    "qcom,mdss-dsi-dispparam-hbm-off-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command-state",
 	"qcom,mdss-dsi-esd-check-read-command-state",
@@ -4987,6 +5021,8 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	if (panel->hbm_mode)
                 dsi_panel_apply_hbm_mode(panel);
 
+    	dsi_panel_set_hbm(panel, false);
+
 	return rc;
 }
 
@@ -5017,6 +5053,10 @@ error:
 	}
 
 	mutex_unlock(&panel->panel_lock);
+    (void)dsi_panel_set_hbm(panel, false);
+
+    if (panel->hbm_requested)
+        (void)dsi_panel_set_hbm(panel, true);
 	return rc;
 }
 
