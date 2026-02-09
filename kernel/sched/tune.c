1217c279416c9c17e4a013c58912462c86e9c7fa
Richard Raya <rdxzv.dev@gmail.com>
2026-02-09 00:05:40 -0300

    sched/tune: Optimize top-app boosting logic
    Change-Id: I9c1e04188df0df61b9eb0ae1ffba413e0821cdf2
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -554,20 +554,23 @@ int schedtune_cpu_boost(int cpu)
 	return bg->boost_max;
 }
 
-static inline bool schedtune_adj_ta(struct schedtune *st, struct task_struct *p)
+static inline int schedtune_adj_ta(struct schedtune *st, struct task_struct *p)
 {
 	char name_buf[NAME_MAX + 1];
 	int adj = p->signal->oom_score_adj;
 
+	if (adj != 0)
+		return 0;
+
+	if (p->flags & PF_KTHREAD)
+		return 0;
+
 	cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
 	if (!strncmp(name_buf, "top-app", strlen("top-app"))) {
-		if ((adj == 0) && !(p->flags & PF_KTHREAD)) {
-			pr_debug("top app is %s\n", p->comm);
-			return true;
-		}
+		return 10;
 	}
 
-	return false;
+	return 0;
 }
 
 int schedtune_task_boost(struct task_struct *p)
@@ -581,8 +584,26 @@ int schedtune_task_boost(struct task_struct *p)
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
-	task_boost = st->boost || schedtune_adj_ta(st, p);
+	task_boost = max(st->boost, schedtune_adj_ta(st, p));
 	rcu_read_unlock();
+
+	return task_boost;
+}
+
+/*  The same as schedtune_task_boost except assuming the caller has the rcu read
+ *  lock.
+ */
+int schedtune_task_boost_rcu_locked(struct task_struct *p)
+{
+	struct schedtune *st;
+	int task_boost;
+
+	if (unlikely(!schedtune_initialized))
+		return 0;
+
+	/* Get task boost value */
+	st = task_schedtune(p);
+	task_boost = max(st->boost, schedtune_adj_ta(st, p));
 
 	return task_boost;
 }
