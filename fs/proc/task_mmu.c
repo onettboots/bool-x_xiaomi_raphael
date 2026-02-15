4058feb5c7608cc08ef3280cab5cf1788a66d6fe
onettboots <blackcocopet@gmail.com>
2026-02-15 18:08:40 +0700

    Revert "[BACKPORT] fs: implement SUSFS v1.5.9"
    This reverts commit 0e1e0e708e4e606bd20e3a1ef4aebf31d6f0a28f.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -502,10 +502,6 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	return 0;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
-#endif
-
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 {
