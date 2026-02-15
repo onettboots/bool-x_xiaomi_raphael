50366a2ab5f913c0afa6989f968ec855f16acdfb
onettboots <blackcocopet@gmail.com>
2026-02-15 19:48:46 +0700

    ksun: test
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -464,6 +464,7 @@ bypass_orig_flow:
 
 	start = vma->vm_start;
 	end = vma->vm_end;
+	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
 	/*
 	 * Print the dentry name for named mappings, and a
