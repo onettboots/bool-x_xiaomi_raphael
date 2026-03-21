bd7fd8f8f6a9eaec76a051ae1a0fce1fc310dea2
atusyarif <atusyaripudin@gmail.com>
2026-03-22 01:37:57 +0700

    Revert "selinux: improve error checking in sel_write_load()"
    This reverts commit d3055d3518590570c9bb0089f35ac1b0a3030d9e.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -536,16 +536,6 @@ static ssize_t sel_write_load(struct file *file, const char __user *buf,
 	ssize_t length;
 	void *data = NULL;
 
-	/* no partial writes */
-	if (*ppos)
-		return -EINVAL;
-	/* no empty policies */
-	if (!count)
-		return -EINVAL;
-
-	if (count > 64 * 1024 * 1024)
-		return -EFBIG;
-
 	mutex_lock(&fsi->mutex);
 
 	length = avc_has_perm(&selinux_state,
@@ -554,15 +544,23 @@ static ssize_t sel_write_load(struct file *file, const char __user *buf,
 	if (length)
 		goto out;
 
+	/* No partial writes. */
+	length = -EINVAL;
+	if (*ppos != 0)
+		goto out;
+
+	length = -EFBIG;
+	if (count > 64 * 1024 * 1024)
+		goto out;
+
+	length = -ENOMEM;
 	data = vmalloc(count);
-	if (!data) {
-		length = -ENOMEM;
+	if (!data)
 		goto out;
-	}
-	if (copy_from_user(data, buf, count) != 0) {
-		length = -EFAULT;
+
+	length = -EFAULT;
+	if (copy_from_user(data, buf, count) != 0)
 		goto out;
-	}
 
 	length = security_load_policy(fsi->state, data, count);
 	if (length) {
@@ -581,7 +579,6 @@ out1:
 		"policy loaded auid=%u ses=%u",
 		from_kuid(&init_user_ns, audit_get_loginuid(current)),
 		audit_get_sessionid(current));
-
 out:
 	mutex_unlock(&fsi->mutex);
 	vfree(data);
