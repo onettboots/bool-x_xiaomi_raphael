e4ed62470313cf412aae9c30e02a6d0a2606cd5a
Richard Raya <rdxzv.dev@gmail.com>
2026-03-24 17:29:51 -0300

    sched/tune: Increase stune group targets
    Change-Id: I06b54107f86d4c531c5063f21165f89941dc5ac3
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -106,7 +106,7 @@ root_schedtune = {
  *    implementation especially for the computation of the per-CPU boost
  *    value
  */
-#define BOOSTGROUPS_COUNT 6
+#define BOOSTGROUPS_COUNT 11
 
 /* Array of configured boostgroups */
 static struct schedtune *allocated_group[BOOSTGROUPS_COUNT] = {
@@ -781,11 +781,16 @@ static void write_default_values(struct cgroup_subsys_state *css)
 {
 	char name_buf[NAME_MAX + 1];
 	static struct st_data st_targets[] = {
-		{ "audio-app",	0, 0 },
-		{ "background",	0, 0 },
-		{ "foreground",	0, 1 },
-		{ "rt",		0, 0 },
-		{ "top-app",	0, 1 },
+		{ "audio-app",		0, 0 },
+		{ "background",		0, 0 },
+		{ "camera-daemon",	0, 0 },
+		{ "foreground",		0, 1 },
+		{ "foreground_window",	0, 1 },
+		{ "nnapi-hal",		0, 0 },
+		{ "restricted",		0, 0 },
+		{ "rt",			0, 0 },
+		{ "system-background",	0, 0 },
+		{ "top-app",		0, 1 },
 	};
 	int i;
 
