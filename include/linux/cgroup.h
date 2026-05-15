508e7f19ea27b1b66cce7776f52c1cfccf4d27fd
onettboots <blackcocopet@gmail.com>
2026-05-15 22:01:02 +0700

    [SQUASH] Revert "BACKPORT: cgroup: export list of cgroups v2 features using sysfs"
    e3d0c6ca7e2c BACKPORT: cgroup: Remove unused cgrp variable
    e6c42835dda2 BACKPORT: cgroup: freezer: call cgroup_enter_frozen() with preemption disabled in ptrace_stop()
    7849050b6f16 BACKPORT: cgroup: freezer: fix frozen state inheritance
    4dfd2e95b146 BACKPORT: signal: unconditionally leave the frozen state in ptrace_stop()
    d5ff087ae554 BACKPORT: cgroup: cgroup v2 freezer
    0b53789e7235 BACKPORT: cgroup: implement __cgroup_task_count() helper
    b92194de45aa BACKPORT: cgroup: rename freezer.c into legacy_freezer.c
    5cc7f6681071 BACKPORT: cgroup: remove extra cgroup_migrate_finish() call
    b9f8a310dde1 BACKPORT: cgroup: saner refcounting for cgroup_root
    21190d723446 BACKPORT: cgroup: Add named hierarchy disabling to cgroup_no_v1 boot param
    ee5bff4d0d2f BACKPORT: cgroup: remove unnecessary unlikely()
    e5e2e2022c0a BACKPORT: cgroup: Simplify cgroup_ancestor
    05f3ae7c23f9 BACKPORT: cgroup: Explicitly remove core interface files
    45c7d4273bd3 BACKPORT: cgroup: Update documentation reference
    66ce35065e64 BACKPORT: cgroup: make cgroup.threads delegatable
    3f1dd21f9e44 BACKPORT: string: drop __must_check from strscpy() and restore strscpy() usages in cgroup
    a2d4935e3dd2 BACKPORT: cgroup: use strlcpy() instead of strscpy() to avoid spurious warning
    a54e931f2610 BACKPORT: cgroup: avoid copying strings longer than the buffers
    470824298f40 BACKPORT: cgroup: export list of cgroups v2 features using sysfs
    This reverts commit 470824298f404e0ce09837885c7cd04e92b8645b.
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -579,11 +579,20 @@ static inline bool cgroup_is_descendant(struct cgroup *cgrp,
 static inline struct cgroup *cgroup_ancestor(struct cgroup *cgrp,
 					     int ancestor_level)
 {
+	struct cgroup *ptr;
+
 	if (cgrp->level < ancestor_level)
 		return NULL;
-	while (cgrp && cgrp->level > ancestor_level)
-		cgrp = cgroup_parent(cgrp);
-	return cgrp;
+
+	for (ptr = cgrp;
+	     ptr && ptr->level > ancestor_level;
+	     ptr = cgroup_parent(ptr))
+		;
+
+	if (ptr && ptr->level == ancestor_level)
+		return ptr;
+
+	return NULL;
 }
 
 /**
@@ -852,46 +861,4 @@ static inline void cgroup_bpf_put(struct cgroup *cgrp) {}
 
 #endif /* CONFIG_CGROUP_BPF */
 
-#ifdef CONFIG_CGROUPS
-
-void cgroup_enter_frozen(void);
-void cgroup_leave_frozen(bool always_leave);
-void cgroup_update_frozen(struct cgroup *cgrp);
-void cgroup_freeze(struct cgroup *cgrp, bool freeze);
-void cgroup_freezer_migrate_task(struct task_struct *task, struct cgroup *src,
-				 struct cgroup *dst);
-void cgroup_freezer_frozen_exit(struct task_struct *task);
-static inline bool cgroup_task_freeze(struct task_struct *task)
-{
-	bool ret;
-
-	if (task->flags & PF_KTHREAD)
-		return false;
-
-	rcu_read_lock();
-	ret = test_bit(CGRP_FREEZE, &task_dfl_cgroup(task)->flags);
-	rcu_read_unlock();
-
-	return ret;
-}
-
-static inline bool cgroup_task_frozen(struct task_struct *task)
-{
-	return task->frozen;
-}
-
-#else /* !CONFIG_CGROUPS */
-
-static inline void cgroup_enter_frozen(void) { }
-static inline void cgroup_leave_frozen(bool always_leave) { }
-static inline bool cgroup_task_freeze(struct task_struct *task)
-{
-	return false;
-}
-static inline bool cgroup_task_frozen(struct task_struct *task)
-{
-	return false;
-}
-
-#endif /* !CONFIG_CGROUPS */
 #endif /* _LINUX_CGROUP_H */
