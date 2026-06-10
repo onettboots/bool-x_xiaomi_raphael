cde61e75ad92c1f66a68ccd3b3be292b680c3428
onettboots <blackcocopet@gmail.com>
2026-06-10 23:03:21 +0700

    Revert "pstore: Introduce max_reason and convert dump_oops"
    This reverts commit 76ee66c25792d157b1330838089532a07e779839.

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -79,10 +79,10 @@ module_param(mem_type, uint, 0400);
 MODULE_PARM_DESC(mem_type,
 		"set to 1 to try to use unbuffered memory (default 0)");
 
-static int ramoops_max_reason = -1;
-module_param_named(max_reason, ramoops_max_reason, int, 0400);
-MODULE_PARM_DESC(max_reason,
-		 "maximum reason for kmsg dump (default 2: Oops and Panic) ");
+static int dump_oops = 1;
+module_param(dump_oops, int, 0400);
+MODULE_PARM_DESC(dump_oops,
+		"set to 1 to dump oopses, 0 to only dump panics (default 1)");
 
 static int ramoops_ecc;
 module_param_named(ecc, ramoops_ecc, int, 0400);
@@ -90,11 +90,6 @@ MODULE_PARM_DESC(ramoops_ecc,
 		"if non-zero, the option enables ECC support and specifies "
 		"ECC buffer size in bytes (1 is a special value, means 16 "
 		"bytes ECC)");
-
-static int ramoops_dump_oops = -1;
-module_param_named(dump_oops, ramoops_dump_oops, int, 0400);
-MODULE_PARM_DESC(dump_oops,
-		 "(deprecated: use max_reason instead) set to 1 to dump oopses & panics, 0 to only dump panics");
 
 struct ramoops_context {
 	struct persistent_ram_zone **dprzs;	/* Oops dump zones */
@@ -108,6 +103,7 @@ struct ramoops_context {
 	size_t console_size;
 	size_t ftrace_size;
 	size_t pmsg_size;
+	int dump_oops;
 	u32 flags;
 	struct persistent_ram_ecc_info ecc_info;
 	unsigned int max_dump_cnt;
@@ -719,14 +715,7 @@ static int ramoops_parse_dt(struct platform_device *pdev,
 	pdata->mem_size = resource_size(res);
 	pdata->mem_address = res->start;
 	pdata->mem_type = of_property_read_bool(of_node, "unbuffered");
-	/*
-	 * Setting "no-dump-oops" is deprecated and will be ignored if
-	 * "max_reason" is also specified.
-	 */
-	if (of_property_read_bool(of_node, "no-dump-oops"))
-		pdata->max_reason = KMSG_DUMP_PANIC;
-	else
-		pdata->max_reason = KMSG_DUMP_OOPS;
+	pdata->dump_oops = !of_property_read_bool(of_node, "no-dump-oops");
 
 #define parse_u32(name, field, default_value) {				\
 		ret = ramoops_parse_dt_u32(pdev, name, default_value,	\
@@ -742,7 +731,6 @@ static int ramoops_parse_dt(struct platform_device *pdev,
 	parse_u32("pmsg-size", pdata->pmsg_size, 0);
 	parse_u32("ecc-size", pdata->ecc_info.ecc_size, 0);
 	parse_u32("flags", pdata->flags, 0);
-	parse_u32("max-reason", pdata->max_reason, pdata->max_reason);
 
 #undef parse_u32
 
@@ -814,6 +802,7 @@ static int ramoops_probe(struct platform_device *pdev)
 	cxt->console_size = pdata->console_size;
 	cxt->ftrace_size = pdata->ftrace_size;
 	cxt->pmsg_size = pdata->pmsg_size;
+	cxt->dump_oops = pdata->dump_oops;
 	cxt->flags = pdata->flags;
 	cxt->ecc_info = pdata->ecc_info;
 
@@ -856,10 +845,8 @@ static int ramoops_probe(struct platform_device *pdev)
 	 * the single region size is how to check.
 	 */
 	cxt->pstore.flags = 0;
-	if (cxt->max_dump_cnt) {
+	if (cxt->max_dump_cnt)
 		cxt->pstore.flags |= PSTORE_FLAGS_DMESG;
-		cxt->pstore.max_reason = pdata->max_reason;
-	}
 	if (cxt->console_size)
 		cxt->pstore.flags |= PSTORE_FLAGS_CONSOLE;
 	if (cxt->max_ftrace_cnt)
@@ -895,7 +882,7 @@ static int ramoops_probe(struct platform_device *pdev)
 	mem_size = pdata->mem_size;
 	mem_address = pdata->mem_address;
 	record_size = pdata->record_size;
-	ramoops_max_reason = pdata->max_reason;
+	dump_oops = pdata->dump_oops;
 	ramoops_console_size = pdata->console_size;
 	ramoops_pmsg_size = pdata->pmsg_size;
 	ramoops_ftrace_size = pdata->ftrace_size;
@@ -970,16 +957,7 @@ static void ramoops_register_dummy(void)
 	dummy_data->console_size = ramoops_console_size;
 	dummy_data->ftrace_size = ramoops_ftrace_size;
 	dummy_data->pmsg_size = ramoops_pmsg_size;
-	/* If "max_reason" is set, its value has priority over "dump_oops". */
-	if (ramoops_max_reason >= 0)
-		dummy_data->max_reason = ramoops_max_reason;
-	/* Otherwise, if "dump_oops" is set, parse it into "max_reason". */
-	else if (ramoops_dump_oops != -1)
-		dummy_data->max_reason = ramoops_dump_oops ? KMSG_DUMP_OOPS
-						     : KMSG_DUMP_PANIC;
-	/* And if neither are explicitly set, use the default. */
-	else
-		dummy_data->max_reason = KMSG_DUMP_OOPS;
+	dummy_data->dump_oops = dump_oops;
 	dummy_data->flags = RAMOOPS_FLAG_FTRACE_PER_CPU;
 
 	/*
